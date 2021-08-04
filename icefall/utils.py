cabe8b625bf55ecabcfb7dc75359d4877c7235c8
pkufool <wkang@pku.org.cn>
2021-08-04 14:27:11 +0800

    Copy the files related to multi round nbest rescoring from k2 & snowfall

--- icefall/utils.py
+++ icefall/utils.py
@@ -5,6 +5,7 @@ import subprocess
 from collections import defaultdict
 from contextlib import contextmanager
 from datetime import datetime
+from nbest import Nbest
 from pathlib import Path
 from typing import Dict, Iterable, List, TextIO, Tuple, Union
 
@@ -381,3 +382,75 @@ def write_error_stats(
 
         print(f"{word}   {corr} {tot_errs} {ref_count} {hyp_count}", file=f)
     return float(tot_err_rate)
+
+
+def get_best_matching_stats(keys: Nbest, queries: Nbest,
+                            max_order: int) -> torch.Tensor:
+    '''Get best matching stats on query positions.
+
+    Args:
+      keys:
+        The nbest after doing second pass rescoring.
+      queries:
+        Another nbest before doing second pass rescoring.
+      max_order:
+        The maximum n-gram order to ever return by `k2.get_best_matching_stats`
+
+    Returns:
+      A tensor with the shape of [queries.fsa.num_elements, 5], each row
+      contains the stats (init_score, mean, var, counts_out, ngram_order)
+      of the token in the correspodding position in queries.
+    '''
+    assert keys.shape.dim0() == queries.shape.dim0(), \
+        f'Utterances number in keys and queries should be equal : \
+         {keys.shape.dim0()} vs {queries.shape.dim0()}'
+
+    # keys_tokens_shape [utt][path][token]
+    keys_tokens_shape = k2.ragged.compose_ragged_shapes(keys.shape,
+        k2.ragged.remove_axis(keys.fsa.arcs.shape(), 1))
+    # queries_tokens_shape [utt][path][token]
+    queries_tokens_shape = k2.ragged.compose_ragged_shapes(queries.shape,
+        k2.ragged.remove_axis(queries.fsa.arcs.shape(), 1))
+
+    keys_tokens = k2.RaggedInt(keys_tokens_shape, keys.fsa.labels.clone())
+    queries_tokens = k2.RaggedInt(queries_tokens_shape,
+                                  queries.fsa.labels.clone())
+    # tokens shape [utt][path][token]
+    tokens = k2.ragged.cat([keys_tokens, queries_tokens], axis=1)
+
+    keys_token_num = keys.fsa.labels.size()[0]
+    queries_tokens_num = queries.fsa.labels.size()[0]
+    # counts on key positions are ones
+    keys_counts = k2.RaggedInt(keys_tokens_shape,
+                               torch.ones(keys_token_num,
+                                          dtype=torch.int32))
+    # counts on query positions are zeros
+    queries_counts = k2.RaggedInt(queries_tokens_shape,
+                                  torch.zeros(queries_tokens_num,
+                                              dtype=torch.int32))
+    counts = k2.ragged.cat([keys_counts, queries_counts], axis=1).values()
+
+    # scores on key positions are the scores inherted from nbest path
+    keys_scores = k2.RaggedFloat(keys_tokens_shape, keys.fsa.scores.clone())
+    # scores on query positions MUST be zeros
+    queries_scores = k2.RaggedFloat(queries_tokens_shape,
+                                    torch.zeros(queries_tokens_num,
+                                                dtype=torch.float32))
+    scores = k2.ragged.cat([keys_scores, queries_scores], axis=1).values()
+
+    # we didn't remove -1 labels before
+    min_token = -1
+    eos = -1
+    max_token = torch.max(torch.max(keys.fsa.labels),
+                          torch.max(queries.fsa.labels))
+    mean, var, counts_out, ngram = k2.get_best_matching_stats(tokens, scores,
+        counts, eos, min_token, max_token, max_order)
+
+    queries_init_scores = queries.fsa.scores.clone()
+    # only return the stats on query positions
+    masking = counts == 0
+    # shape [queries_tokens_num, 5]
+    return torch.transpose(torch.stack((queries_init_scores, mean[masking],
+                                        var[masking], counts_out[masking],
+                                        ngram[masking])), 0, 1)
+
