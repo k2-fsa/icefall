19c4214958768f79dd13c89f1f05e52d35b5dce2
pkufool <wkang@pku.org.cn>
2021-08-23 10:43:59 +0800

    Fix code style and add copyright. (#18)
    * Fix style and add copyright
    
    * Minor fix
    
    * Remove duplicate lines
    
    * Reformat conformer.py by black
    
    * Reformat code style with black.
    
    * Fix github workflows
    
    * Fix lhotse installation
    
    * Install icefall requirements
    
    * Update k2 version, remove lhotse from test workflow

--- egs/librispeech/ASR/local/compute_fbank_librispeech.py
+++ egs/librispeech/ASR/local/compute_fbank_librispeech.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+# Copyright    2021  Xiaomi Corp.        (authors: Fangjun Kuang)
+#
+# See ../../../../LICENSE for clarification regarding multiple authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 """
 This file computes fbank features of the LibriSpeech dataset.
@@ -17,8 +33,9 @@ from lhotse.recipes.utils import read_manifests_if_cached
 
 from icefall.utils import get_executor
 
-# Torch's multithreaded behavior needs to be disabled or it wastes a lot of CPU and
-# slow things down.  Do this outside of main() in case it needs to take effect
+# Torch's multithreaded behavior needs to be disabled or
+# it wastes a lot of CPU and slow things down.
+# Do this outside of main() in case it needs to take effect
 # even when we are not invoking the main (e.g. when spawning subprocesses).
 torch.set_num_threads(1)
 torch.set_num_interop_threads(1)
@@ -53,7 +70,8 @@ def compute_fbank_librispeech():
                 continue
             logging.info(f"Processing {partition}")
             cut_set = CutSet.from_manifests(
-                recordings=m["recordings"], supervisions=m["supervisions"],
+                recordings=m["recordings"],
+                supervisions=m["supervisions"],
             )
             if "train" in partition:
                 cut_set = (
