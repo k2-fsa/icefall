9176da75745e2e5cb354f43a06af93189f3cbef6
Mingshuang Luo <37799481+luomingshuang@users.noreply.github.com>
2021-10-14 19:21:02 +0800

    Update pretrained.py

--- egs/librispeech/ASR/conformer_ctc/pretrained.py
+++ egs/librispeech/ASR/conformer_ctc/pretrained.py
@@ -57,22 +57,19 @@ def get_parser():
     parser.add_argument(
         "--words-file",
         type=str,
-        help="Path to words.txt "
-        "Used only when method is not ctc-decoding",
+        help="Path to words.txt " "Used only when method is not ctc-decoding",
     )
 
     parser.add_argument(
         "--HLG",
         type=str,
-        help="Path to HLG.pt. "
-        "Used only when method is not ctc-decoding",
+        help="Path to HLG.pt. " "Used only when method is not ctc-decoding",
     )
 
     parser.add_argument(
         "--bpe-model",
         type=str,
-        help="Path to bpe.model. "
-        "Used only when method is ctc-decoding.",
+        help="Path to bpe.model. " "Used only when method is ctc-decoding.",
     )
 
     parser.add_argument(
