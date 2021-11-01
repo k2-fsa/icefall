bf98c0fd27af2cbcfacf63376dd76c7973e9937e
Guo Liyong <guonwpu@qq.com>
2021-11-01 19:29:33 +0800

    fix style check

--- egs/librispeech/ASR/conformer_ctc/gigaspeech_datamodule.py
+++ egs/librispeech/ASR/conformer_ctc/gigaspeech_datamodule.py
@@ -26,11 +26,9 @@ from lhotse import CutSet, Fbank, FbankConfig, load_manifest
 from lhotse.dataset import (
     BucketingSampler,
     CutConcatenate,
-    CutMix,
     K2SpeechRecognitionDataset,
     PrecomputedFeatures,
     SingleCutSampler,
-    SpecAugment,
 )
 from lhotse.dataset.input_strategies import OnTheFlyFeatures
 from torch.utils.data import DataLoader
@@ -195,7 +193,6 @@ class GigaSpeechAsrDataModule(DataModule):
                 input_strategy=OnTheFlyFeatures(
                     Fbank(FbankConfig(num_mel_bins=80))
                 ),
-                input_transforms=input_transforms,
                 return_cuts=self.args.return_cuts,
             )
 
@@ -321,15 +318,11 @@ class GigaSpeechAsrDataModule(DataModule):
     @lru_cache()
     def valid_cuts(self) -> CutSet:
         logging.info("About to get dev cuts")
-        cuts_valid = load_manifest(
-            self.args.feature_dir / "cuts_DEV.json.gz"
-        )
+        cuts_valid = load_manifest(self.args.feature_dir / "cuts_DEV.json.gz")
         return cuts_valid
 
     @lru_cache()
     def test_cuts(self) -> List[CutSet]:
         logging.info("About to get dev cuts")
-        cuts_test = load_manifest(
-            self.args.feature_dir / "cuts_TEST.json.gz"
-        )
+        cuts_test = load_manifest(self.args.feature_dir / "cuts_TEST.json.gz")
         return cuts_test
