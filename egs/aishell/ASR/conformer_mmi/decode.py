cf50e16047c76c6c44a81f30bab0417b92028d8c
PingFeng Luo <pingfengluo@gmail.com>
2021-12-01 18:19:03 +0800

    export model

--- egs/aishell/ASR/conformer_mmi/decode.py
+++ egs/aishell/ASR/conformer_mmi/decode.py
@@ -542,8 +542,14 @@ def main():
             if start >= 0:
                 filenames.append(f"{params.exp_dir}/epoch-{i}.pt")
         logging.info(f"averaging {filenames}")
-        model.to(device)
-        model.load_state_dict(average_checkpoints(filenames, device=device))
+        model.load_state_dict(average_checkpoints(filenames))
+
+    if params.export:
+        logging.info(f"Export averaged model to {params.exp_dir}/pretrained.pt")
+        torch.save(
+            {"model": model.state_dict()}, f"{params.exp_dir}/pretrained.pt"
+        )
+        return
 
     model.to(device)
     model.eval()
