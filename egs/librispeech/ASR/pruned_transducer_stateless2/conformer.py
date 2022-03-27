2cde99509fda0dc6fec55eab7504cacc44b6c0fc
Daniel Povey <dpovey@gmail.com>
2022-03-27 23:21:42 +0800

    Change max-keep-prob to 0.95

--- egs/librispeech/ASR/pruned_transducer_stateless2/conformer.py
+++ egs/librispeech/ASR/pruned_transducer_stateless2/conformer.py
@@ -231,7 +231,7 @@ class ConformerEncoderLayer(nn.Module):
         # compensate for the small scale by just producing larger output.
         warmup = max(warmup, 0.1)
         if self.training:
-            warmup = min(warmup, 0.99)  # effectively, layer-drop with 1-in-100 prob.
+            warmup = min(warmup, 0.95)  # effectively, layer-drop with 1-in-20 prob.
         alpha = 1.0 if torch.rand(()).item() <= warmup else 0.1
 
         # macaron style feed forward module
