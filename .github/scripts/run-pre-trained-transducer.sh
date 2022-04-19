e9a3e8376c563e36e75d77228307ca6b8f3c48e9
Fangjun Kuang <csukuangfj@gmail.com>
2022-04-19 16:57:17 +0800

    Refactor CI scripts.

---
+++ .github/scripts/run-pre-trained-transducer.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+log() {
+  # This function is from espnet
+  local fname=${BASH_SOURCE[1]##*/}
+  echo -e "$(date '+%Y-%m-%d %H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+
+cd egs/librispeech/ASR
+
+repo_url=https://huggingface.co/csukuangfj/icefall-asr-librispeech-transducer-bpe-500-2021-12-23
+
+log "Downloading pre-trained model from $repo_url"
+git lfs install
+git clone $repo_url
+repo=$(basename $repo_url)
+
+log "Display test files"
+tree $repo/
+soxi $repo/test_wavs/*.wav
+ls -lh $repo/test_wavs/*.wav
+
+log "Beam search decoding"
+
+./transducer/pretrained.py \
+  --method beam_search \
+  --beam-size 4 \
+  --checkpoint $repo/exp/pretrained.pt \
+  --bpe-model $repo/data/lang_bpe_500/bpe.model \
+  $repo/test_wavs/1089-134686-0001.wav \
+  $repo/test_wavs/1221-135766-0001.wav \
+  $repo/test_wavs/1221-135766-0002.wav
