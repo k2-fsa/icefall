fb8e926521d027c6b2fa602e5c56e2e8f27e1274
yaozengwei <yaozengwei@outlook.com>
2022-05-21 20:53:40 +0800

    fix decode.py

--- egs/librispeech/ASR/conv_emformer_transducer_stateless/decode.py
+++ egs/librispeech/ASR/conv_emformer_transducer_stateless/decode.py
@@ -286,6 +286,7 @@ def decode_one_batch(
         hyp_tokens = greedy_search_batch(
             model=model,
             encoder_out=encoder_out,
+            encoder_out_lens=encoder_out_lens,
         )
         for hyp in sp.decode(hyp_tokens):
             hyps.append(hyp.split())
