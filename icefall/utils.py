accf0afb45407d131a15c7033949992fe842f29e
Fangjun Kuang <csukuangfj@gmail.com>
2022-06-05 22:38:46 +0800

    Remove force=True from logging to support Python < 3.8

--- icefall/utils.py
+++ icefall/utils.py
@@ -131,7 +131,6 @@ def setup_logger(
         format=formatter,
         level=level,
         filemode="w",
-        force=True,
     )
     if use_console:
         console = logging.StreamHandler()
