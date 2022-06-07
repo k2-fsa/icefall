87a355e3762467ddb6b23ea220e30d454846e1bd
Fangjun Kuang <csukuangfj@gmail.com>
2022-06-07 17:40:05 +0800

    Replace LilcomChunkyWriter with ChunkedLilcomHdf5Writer.

--- egs/timit/ASR/local/compute_fbank_timit.py
+++ egs/timit/ASR/local/compute_fbank_timit.py
@@ -29,7 +29,7 @@ import os
 from pathlib import Path
 
 import torch
-from lhotse import CutSet, Fbank, FbankConfig, LilcomChunkyWriter
+from lhotse import ChunkedLilcomHdf5Writer, CutSet, Fbank, FbankConfig
 from lhotse.recipes.utils import read_manifests_if_cached
 
 from icefall.utils import get_executor
@@ -88,7 +88,7 @@ def compute_fbank_timit():
                 # when an executor is specified, make more partitions
                 num_jobs=num_jobs if ex is None else 80,
                 executor=ex,
-                storage_type=LilcomChunkyWriter,
+                storage_type=ChunkedLilcomHdf5Writer,
             )
             cut_set.to_file(cuts_file)
 
