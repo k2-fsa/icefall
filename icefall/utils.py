2cb1618c955b75fe43ab4709b507d4151b1bb03d
Mingshuang Luo <37799481+luomingshuang@users.noreply.github.com>
2022-06-28 11:02:10 +0800

    [Ready to merge] Pruned transducer stateless5 recipe for tal_csasr (mix Chinese chars and English BPE) (#428)
    * add pruned transducer stateless5 recipe for tal_csasr
    
    * do some changes for merging
    
    * change for conformer.py
    
    * add wer and cer for Chinese and English respectively
    
    * fix a error for conformer.py

--- icefall/utils.py
+++ icefall/utils.py
@@ -20,6 +20,7 @@ import argparse
 import collections
 import logging
 import os
+import re
 import subprocess
 from collections import defaultdict
 from contextlib import contextmanager
@@ -30,6 +31,7 @@ from typing import Dict, Iterable, List, TextIO, Tuple, Union
 import k2
 import k2.version
 import kaldialign
+import sentencepiece as spm
 import torch
 import torch.distributed as dist
 import torch.nn as nn
@@ -876,3 +878,40 @@ def load_averaged_model(
     model.load_state_dict(average_checkpoints(filenames, device=device))
 
     return model
+
+
+def tokenize_by_bpe_model(
+    sp: spm.SentencePieceProcessor,
+    txt: str,
+) -> str:
+    """
+    Tokenize text with bpe model. This function is from
+    https://github1s.com/wenet-e2e/wenet/blob/main/wenet/dataset/processor.py#L322-L342.
+    Args:
+      sp: spm.SentencePieceProcessor.
+      txt: str
+
+    Return:
+      A new string which includes chars and bpes.
+    """
+    tokens = []
+    # CJK(China Japan Korea) unicode range is [U+4E00, U+9FFF], ref:
+    # https://en.wikipedia.org/wiki/CJK_Unified_Ideographs_(Unicode_block)
+    pattern = re.compile(r"([\u4e00-\u9fff])")
+    # Example:
+    #   txt   = "你好 ITS'S OKAY 的"
+    #   chars = ["你", "好", " ITS'S OKAY ", "的"]
+    chars = pattern.split(txt.upper())
+    mix_chars = [w for w in chars if len(w.strip()) > 0]
+    for ch_or_w in mix_chars:
+        # ch_or_w is a single CJK charater(i.e., "你"), do nothing.
+        if pattern.fullmatch(ch_or_w) is not None:
+            tokens.append(ch_or_w)
+        # ch_or_w contains non-CJK charaters(i.e., " IT'S OKAY "),
+        # encode ch_or_w using bpe_model.
+        else:
+            for p in sp.encode_as_pieces(ch_or_w):
+                tokens.append(p)
+    txt_with_bpe = "/".join(tokens)
+
+    return txt_with_bpe
