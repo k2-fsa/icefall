b441dffa836302de922ef3a725e30350206e0f4d
Quandwang <quandwang@hotmail.com>
2022-07-20 17:46:40 +0800

    fix typos and modify the expr of ScaledEmbedding

--- egs/librispeech/ASR/conformer_ctc2/train.py
+++ egs/librispeech/ASR/conformer_ctc2/train.py
@@ -990,7 +990,7 @@ def run(rank, world_size, args):
         train_cuts, sampler_state_dict=sampler_state_dict
     )
 
-    valid_cuts = librispeech.dev_cuts()
+    valid_cuts = librispeech.dev_clean_cuts()
     valid_cuts += librispeech.dev_other_cuts()
     valid_dl = librispeech.valid_dataloaders(valid_cuts)
 
