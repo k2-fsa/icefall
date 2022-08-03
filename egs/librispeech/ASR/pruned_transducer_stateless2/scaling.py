58a96e5b681a1db8b0029f4627b4d160af78054a
Fangjun Kuang <csukuangfj@gmail.com>
2022-08-03 10:30:28 +0800

    Support exporting to ONNX format (#501)
    * WIP: Support exporting to ONNX format
    
    * Minor fixes.
    
    * Combine encoder/decoder/joiner into a single file.
    
    * Revert merging three onnx models into a single one.
    
    It's quite time consuming to extract a sub-graph from the combined
    model. For instance, it takes more than one hour to extract
    the encoder model.
    
    * Update CI to test ONNX models.
    
    * Decode with exported models.
    
    * Fix typos.
    
    * Add more doc.
    
    * Remove ncnn as it is not fully tested yet.
    
    * Fix as_strided for streaming conformer.

--- egs/librispeech/ASR/pruned_transducer_stateless2/scaling.py
+++ egs/librispeech/ASR/pruned_transducer_stateless2/scaling.py
@@ -152,7 +152,8 @@ class BasicNorm(torch.nn.Module):
             self.register_buffer("eps", torch.tensor(eps).log().detach())
 
     def forward(self, x: Tensor) -> Tensor:
-        assert x.shape[self.channel_dim] == self.num_channels
+        if not torch.jit.is_tracing():
+            assert x.shape[self.channel_dim] == self.num_channels
         scales = (
             torch.mean(x ** 2, dim=self.channel_dim, keepdim=True)
             + self.eps.exp()
@@ -423,7 +424,7 @@ class ActivationBalancer(torch.nn.Module):
         self.max_abs = max_abs
 
     def forward(self, x: Tensor) -> Tensor:
-        if torch.jit.is_scripting():
+        if torch.jit.is_scripting() or torch.jit.is_tracing():
             return x
         else:
             return ActivationBalancerFunction.apply(
@@ -472,7 +473,7 @@ class DoubleSwish(torch.nn.Module):
         """Return double-swish activation function which is an approximation to Swish(Swish(x)),
         that we approximate closely with x * sigmoid(x-1).
         """
-        if torch.jit.is_scripting():
+        if torch.jit.is_scripting() or torch.jit.is_tracing():
             return x * torch.sigmoid(x - 1.0)
         else:
             return DoubleSwishFunction.apply(x)
