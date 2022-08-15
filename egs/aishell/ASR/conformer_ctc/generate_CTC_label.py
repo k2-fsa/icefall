bf5c07381f7bfc06678209dfcfd95e2b594387da
qmpzzpmq <405691733@qq.com>
2022-08-15 14:57:06 +0000

    author

--- egs/aishell/ASR/conformer_ctc/generate_CTC_label.py
+++ egs/aishell/ASR/conformer_ctc/generate_CTC_label.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2021 Xiaomi Corporation (Author: Liyong Guo,
-#                                            Fangjun Kuang,
-#                                            Wei Kang)
-#
-# See ../../../../LICENSE for clarification regarding multiple authors
+# Author: Haoyu Tang
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
