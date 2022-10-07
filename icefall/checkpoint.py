28e5f468545e7edf96037735f4dfdc835d3b5c7c
Daniel Povey <dpovey@gmail.com>
2022-10-07 17:06:38 +0800

    Update checkpoint.py to deal with int params

--- icefall/checkpoint.py
+++ icefall/checkpoint.py
@@ -466,8 +466,10 @@ def average_state_dict(
 
     uniqued_names = list(uniqued.values())
     for k in uniqued_names:
-        state_dict_1[k] *= weight_1
-        state_dict_1[k] += (
-            state_dict_2[k].to(device=state_dict_1[k].device) * weight_2
-        )
-        state_dict_1[k] *= scaling_factor
+        v = state_dict_1[k]
+        if torch.is_floating_point(v):
+            v *= weight_1
+            v += (
+                state_dict_2[k].to(device=state_dict_1[k].device) * weight_2
+            )
+            v *= scaling_factor
