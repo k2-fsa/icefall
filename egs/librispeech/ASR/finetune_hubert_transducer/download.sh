c05bc681119deb0b7c111b951e52129103541da6
marcoyang <marcoyang1998@gmail.com>
2022-10-08 16:58:14 +0800

    also download dict file

--- egs/librispeech/ASR/finetune_hubert_transducer/download.sh
+++ egs/librispeech/ASR/finetune_hubert_transducer/download.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+# Usage:
+# Navigate to /path/to/egs/lirbispeech/ASR, then call
+# ./finetune_hubert_transducer/download.sh model_type
+# replace model_type with the model type you want
+# Available types are [base, large, xlarge]
+# For example, if you want to download Hubert large, run:
+# ./finetune_hubert_transducer/download.sh large
+
 model_type=$1
 
 download_dir="pretrained_models"
 cd finetune_hubert_transducer
 mkdir -p ${download_dir}
 
+# download pretrained models
 if [ "${model_type}" = "base" ]; then
     if [ -f "${download_dir}/hubert_base_ls960.pt" ]; then
         echo "File already exist, skip downloading"
@@ -30,6 +39,10 @@ elif [ "${model_type}" = "xlarge" ]; then
 else
     echo "Unknown Hubert type: ${model_type}"
 fi
+
+# download dict file
+wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/dict.ltr.txt -P ${download_dir}
+
 
 # url_dict = {
 #         "base": "https://dl.fbaipublicfiles.com/hubert/hubert_base_ls960.pt",
