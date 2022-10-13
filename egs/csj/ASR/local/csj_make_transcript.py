6d58d9d1e16979929f96f49dcf8d6735ae0faa49
Teo Wen Shen <teouuenshen@gmail.com>
2022-10-13 22:07:54 +0900

    Add header

--- egs/csj/ASR/local/csj_make_transcript.py
+++ egs/csj/ASR/local/csj_make_transcript.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python3
+# Copyright    2022  The University of Electro-Communications  (Author: Teo Wen Shen)  # noqa
+#
+# See ../../../../LICENSE for clarification regarding multiple authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import argparse
 import logging
 import os
@@ -801,8 +819,10 @@ def load_config(config_file: Path):
     config = ConfigParser()
     config.optionxform = str
     config.read(config_file)
+    # fmt: off
     global PLUS, DECISIONS_PRON, DECISIONS_SURFACE, REPLACEMENTS_PRON, \
         REPLACEMENTS_SURFACE, MORPH, FIELDS, JPN_NUM, KATAKANA2ROMAJI
+    # fmt: on
     PLUS = config["CONSTANTS"]["PLUS"]
     MORPH = config["CONSTANTS"]["MORPH"].split()
     JPN_NUM = config["CONSTANTS"]["JPN_NUM"].split()
