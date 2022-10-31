b806a21fa92ea36223f5168af93524b8801310b3
Daniel Povey <dpovey@gmail.com>
2022-10-31 20:25:42 +0800

    Remove the 5th of 6 encoder stacks

--- egs/librispeech/ASR/pruned_transducer_stateless7/train.py
+++ egs/librispeech/ASR/pruned_transducer_stateless7/train.py
@@ -104,35 +104,35 @@ def add_model_arguments(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--num-encoder-layers",
         type=str,
-        default="2,4,3,2,2,4",
+        default="2,4,3,2,4",
         help="Number of zipformer encoder layers, comma separated.",
     )
 
     parser.add_argument(
         "--feedforward-dims",
         type=str,
-        default="1024,1024,1536,1536,1536,1024",
+        default="1024,1024,1536,1536,1024",
         help="Feedforward dimension of the zipformer encoder layers, comma separated.",
     )
 
     parser.add_argument(
         "--nhead",
         type=str,
-        default="8,8,8,8,8,8",
+        default="8,8,8,8,8",
         help="Number of attention heads in the zipformer encoder layers.",
     )
 
     parser.add_argument(
         "--encoder-dims",
         type=str,
-        default="384,384,384,384,384,384",
+        default="384,384,384,384,384",
         help="Embedding dimension in the 2 blocks of zipformer encoder layers, comma separated"
     )
 
     parser.add_argument(
         "--attention-dims",
         type=str,
-        default="192,192,192,192,192,192",
+        default="192,192,192,192,192",
         help="""Attention dimension in the 2 blocks of zipformer encoder layers, comma separated;
         not the same as embedding dimension."""
     )
@@ -140,7 +140,7 @@ def add_model_arguments(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--encoder-unmasked-dims",
         type=str,
-        default="256,256,256,256,256,256",
+        default="256,256,256,256,256",
         help="Unmasked dimensions in the encoders, relates to augmentation during training.  "
         "Must be <= each of encoder_dims.  Empirically, less than 256 seems to make performance "
         " worse."
@@ -149,14 +149,14 @@ def add_model_arguments(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--zipformer-downsampling-factors",
         type=str,
-        default="1,2,4,8,4,2",
+        default="1,2,4,8,2",
         help="Downsampling factor for each stack of encoder layers.",
     )
 
     parser.add_argument(
         "--cnn-module-kernels",
         type=str,
-        default="31,31,31,31,31,31",
+        default="31,31,31,31,31",
         help="Sizes of kernels in convolution modules",
     )
 
