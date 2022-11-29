441fcf063de81e933a75c87141b43a7356127b15
Daniel Povey <dpovey@gmail.com>
2022-11-29 16:15:34 +0800

    Reduce final value of bypass_min from 0.25 to 0.2

--- egs/librispeech/ASR/pruned_transducer_stateless7/zipformer.py
+++ egs/librispeech/ASR/pruned_transducer_stateless7/zipformer.py
@@ -389,7 +389,7 @@ class ZipformerEncoderLayer(nn.Module):
             layer_skip_rate: FloatLike = ScheduledFloat((0.0, 0.5), (4000.0, 0.05), default=0),
             dynamic_skip_rate: FloatLike = ScheduledFloat((0.0, 0.2), (4000.0, 0.0), default=0),
             const_attention_rate: FloatLike = ScheduledFloat((0.0, 0.25), (4000.0, 0.025), default=0),
-            bypass_min: FloatLike = ScheduledFloat((0.0, 0.75), (20000.0, 0.25), default=0),
+            bypass_min: FloatLike = ScheduledFloat((0.0, 0.75), (20000.0, 0.2), default=0),
             bypass_max: FloatLike = 1.0,
     ) -> None:
         super(ZipformerEncoderLayer, self).__init__()
