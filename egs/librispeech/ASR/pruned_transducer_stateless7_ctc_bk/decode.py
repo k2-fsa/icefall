385cd839b21a5cdb347ca042d1e3c4eee0800f5d
yifanyang <yifanyeung@yifanyangs-MacBook-Pro.local>
2022-12-03 14:21:48 +0800

    Add warmup to blank skip

--- egs/librispeech/ASR/pruned_transducer_stateless7_ctc_bk/decode.py
+++ egs/librispeech/ASR/pruned_transducer_stateless7_ctc_bk/decode.py
@@ -19,36 +19,36 @@
 """
 Usage:
 (1) greedy search
-./pruned_transducer_stateless7_ctc/decode.py \
+./pruned_transducer_stateless7_ctc_bk/decode.py \
     --epoch 28 \
     --avg 15 \
-    --exp-dir ./pruned_transducer_stateless7_ctc/exp \
+    --exp-dir ./pruned_transducer_stateless7_ctc_bk/exp \
     --max-duration 600 \
     --decoding-method greedy_search
 
 (2) beam search (not recommended)
-./pruned_transducer_stateless7_ctc/decode.py \
+./pruned_transducer_stateless7_ctc_bk/decode.py \
     --epoch 28 \
     --avg 15 \
-    --exp-dir ./pruned_transducer_stateless7_ctc/exp \
+    --exp-dir ./pruned_transducer_stateless7_ctc_bk/exp \
     --max-duration 600 \
     --decoding-method beam_search \
     --beam-size 4
 
 (3) modified beam search
-./pruned_transducer_stateless7_ctc/decode.py \
+./pruned_transducer_stateless7_ctc_bk/decode.py \
     --epoch 28 \
     --avg 15 \
-    --exp-dir ./pruned_transducer_stateless7_ctc/exp \
+    --exp-dir ./pruned_transducer_stateless7_ctc_bk/exp \
     --max-duration 600 \
     --decoding-method modified_beam_search \
     --beam-size 4
 
 (4) fast beam search (one best)
-./pruned_transducer_stateless7_ctc/decode.py \
+./pruned_transducer_stateless7_ctc_bk/decode.py \
     --epoch 28 \
     --avg 15 \
-    --exp-dir ./pruned_transducer_stateless7_ctc/exp \
+    --exp-dir ./pruned_transducer_stateless7_ctc_bk/exp \
     --max-duration 600 \
     --decoding-method fast_beam_search \
     --beam 20.0 \
@@ -56,10 +56,10 @@ Usage:
     --max-states 64
 
 (5) fast beam search (nbest)
-./pruned_transducer_stateless7_ctc/decode.py \
+./pruned_transducer_stateless7_ctc_bk/decode.py \
     --epoch 28 \
     --avg 15 \
-    --exp-dir ./pruned_transducer_stateless7_ctc/exp \
+    --exp-dir ./pruned_transducer_stateless7_ctc_bk/exp \
     --max-duration 600 \
     --decoding-method fast_beam_search_nbest \
     --beam 20.0 \
@@ -69,10 +69,10 @@ Usage:
     --nbest-scale 0.5
 
 (6) fast beam search (nbest oracle WER)
-./pruned_transducer_stateless7_ctc/decode.py \
+./pruned_transducer_stateless7_ctc_bk/decode.py \
     --epoch 28 \
     --avg 15 \
-    --exp-dir ./pruned_transducer_stateless7_ctc/exp \
+    --exp-dir ./pruned_transducer_stateless7_ctc_bk/exp \
     --max-duration 600 \
     --decoding-method fast_beam_search_nbest_oracle \
     --beam 20.0 \
@@ -82,10 +82,10 @@ Usage:
     --nbest-scale 0.5
 
 (7) fast beam search (with LG)
-./pruned_transducer_stateless7_ctc/decode.py \
+./pruned_transducer_stateless7_ctc_bk/decode.py \
     --epoch 28 \
     --avg 15 \
-    --exp-dir ./pruned_transducer_stateless7_ctc/exp \
+    --exp-dir ./pruned_transducer_stateless7_ctc_bk/exp \
     --max-duration 600 \
     --decoding-method fast_beam_search_nbest_LG \
     --beam 20.0 \
@@ -183,7 +183,7 @@ def get_parser():
     parser.add_argument(
         "--exp-dir",
         type=str,
-        default="pruned_transducer_stateless7_ctc/exp",
+        default="pruned_transducer_stateless7_ctc_bk/exp",
         help="The experiment dir",
     )
 
