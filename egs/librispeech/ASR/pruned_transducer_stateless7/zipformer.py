074f94f25661efa4ec3ae8be897104f74f9e8e77
Daniel Povey <dpovey@gmail.com>
2022-12-03 14:47:36 +0800

    Increase ConvolutionModule min_abs from 0.4 to 1.0

--- egs/librispeech/ASR/pruned_transducer_stateless7/zipformer.py
+++ egs/librispeech/ASR/pruned_transducer_stateless7/zipformer.py
@@ -1600,7 +1600,7 @@ class ConvolutionModule(nn.Module):
             channels, channel_dim=1,
             min_positive=ScheduledFloat((0.0, 0.1), (8000.0, 0.05)),
             max_positive=1.0,
-            min_abs=0.4,
+            min_abs=1.0,
             max_abs=10.0,
         )
 
