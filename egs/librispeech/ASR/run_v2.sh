561d018f6b42ea062172ddbc49ef33acb01eb594
dohe0342 <kimdohe1070@gmail.com>
2022-12-09 17:10:31 +0900

    from local

---
+++ egs/librispeech/ASR/run_v2.sh
@@ -0,0 +1,12 @@
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+
+./pruned_transducer_stateless7_ctc/train.py \
+  --world-size 4 \
+  --num-epochs 30 \
+  --full-libri 1 \
+  --use-fp16 1 \
+  --max-duration 750 \
+  --exp-dir pruned_transducer_stateless7_ctc/exp \
+  --feedforward-dims  "1024,1024,2048,2048,1024" \
+  --ctc-loss-scale 0.2 \
+  --master-port 12535
