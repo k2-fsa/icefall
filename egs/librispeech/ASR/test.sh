3c1957c20b9fcfd448837fc6c14e9b410ed648a8
dohe0342 <kimdohe1070@gmail.com>
2022-12-09 15:58:01 +0900

    from local

---
+++ egs/librispeech/ASR/test.sh
@@ -0,0 +1,19 @@
+git pull
+
+for method in greedy_search modified_beam_search fast_beam_search; do
+  ./pruned_transducer_stateless_d2v/decode.py \
+	--input-strategy AudioSamples \
+	--enable-spec-aug False \
+	--additional-block True \
+	--epoch 14 \
+    --avg 1 \
+	--exp-dir ./pruned_transducer_stateless_d2v/test \
+    --max-duration 800 \
+    --decoding-method $method \
+    --max-sym-per-frame 1 \
+	--encoder-type d2v \
+    --encoder-dim 768 \
+    --decoder-dim 768 \
+    --joiner-dim 768 \
+    --use-averaged-model False
+done
