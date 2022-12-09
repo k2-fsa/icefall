fac31c76e92ededbd9b42aa579f931ac1b1f2152
dohe0342 <kimdohe1070@gmail.com>
2022-12-09 17:22:49 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train.py
@@ -672,7 +672,7 @@ def compute_loss(
     device = model.device if isinstance(model, DDP) else next(model.parameters()).device
     feature = batch["inputs"]
     # at entry, feature is (N, T, C)
-    assert feature.ndim == 3
+    assert feature.ndim == 2 or feature.ndim == 3
     feature = feature.to(device)
 
     supervisions = batch["supervisions"]
