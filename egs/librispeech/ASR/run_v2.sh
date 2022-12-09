9efa39bb39c6fb7972be1f1feef37b1dc915f1a9
dohe0342 <kimdohe1070@gmail.com>
2022-12-09 17:12:17 +0900

    from local

--- egs/librispeech/ASR/run_v2.sh
+++ egs/librispeech/ASR/run_v2.sh
@@ -8,7 +8,7 @@ git pull
   --full-libri 1 \
   --use-fp16 1 \
   --max-duration 300 \
-  --exp-dir pruned_transducer_stateless7_ctc/exp \
+  --exp-dir pruned_transducer_stateless_d2v_v2/exp \
   --feedforward-dims  "1024,1024,2048,2048,1024" \
   --ctc-loss-scale 0.2 \
   --master-port 12535
