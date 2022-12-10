43cde32790e4be0b5208bbadda2003223d7a9d75
dohe0342 <kimdohe1070@gmail.com>
2022-12-10 14:22:02 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train.py
@@ -1183,6 +1183,8 @@ def run(rank, world_size, args, wb=None):
                 dec_names.append(n)
                 dec_param.append(p)
 
+        logging.info(f"{len(enc_names)}")
+        logging.info(f"{len(enc_param)}")
         optimizer_enc = ScaledAdam(
             enc_param,
             lr=params.peak_enc_lr,
