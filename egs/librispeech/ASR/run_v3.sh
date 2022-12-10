c6ae840c919d35c890cb3390e4cdc2294413ed38
dohe0342 <kimdohe1070@gmail.com>
2022-12-10 16:24:25 +0900

    from local

--- egs/librispeech/ASR/run_v3.sh
+++ egs/librispeech/ASR/run_v3.sh
@@ -16,7 +16,7 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 	--freeze-finetune-updates 2000 \
 	--use-fp16 1 \
 	--peak-enc-lr 0.001 \
-	--peak-dec-lr 0.03 \
+	--peak-dec-lr 0.05 \
 	--accum-grads 1 \
 	--encoder-type d2v \
 	--additional-block True \
