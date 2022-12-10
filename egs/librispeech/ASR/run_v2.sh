c6d51efa6957ce5ef5db6e873e2440a6745d8807
dohe0342 <kimdohe1070@gmail.com>
2022-12-10 11:54:48 +0900

    from local

--- egs/librispeech/ASR/run_v2.sh
+++ egs/librispeech/ASR/run_v2.sh
@@ -3,8 +3,6 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
 git pull
 
 ./pruned_transducer_stateless_d2v_v2/train.py \
-	--input-strategy AudioSamples \
-	--enable-spec-aug False \
 	--world-size 8 \
 	--num-epochs 30 \
 	--full-libri 1 \
@@ -15,3 +13,6 @@ git pull
 	--feedforward-dims  "1024,1024,2048,2048,1024" \
 	--ctc-loss-scale 0.0 \
 	--master-port 12535
+#--input-strategy AudioSamples \
+#--enable-spec-aug False \
+
