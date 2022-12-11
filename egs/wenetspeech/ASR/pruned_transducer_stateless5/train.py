27c0fdf1781ed154e3ad89e34a90c3e0668753f5
v-yunbin <yunbzhang@126.com>
2022-12-11 19:48:32 +0800

    fix style issues

--- egs/wenetspeech/ASR/pruned_transducer_stateless5/train.py
+++ egs/wenetspeech/ASR/pruned_transducer_stateless5/train.py
@@ -1014,7 +1014,7 @@ def run(rank, world_size, args):
         # You should use ../local/display_manifest_statistics.py to get
         # an utterance duration distribution for your dataset to select
         # the threshold
-        if c.duration < 1.0 or c.duration > 20.0:
+        if c.duration < 1.0 or c.duration > 10.0:
             logging.warning(
                 f"Exclude cut with ID {c.id} from training. Duration: {c.duration}"
             )
