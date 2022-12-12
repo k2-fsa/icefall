ce9a9396b8762f1116e907268e4fe3e9a525b5bc
dohe0342 <kimdohe1070@gmail.com>
2022-12-13 02:07:27 +0900

    from local

--- egs/librispeech/ASR/run_v3.sh
+++ egs/librispeech/ASR/run_v3.sh
@@ -18,7 +18,7 @@ if [ $workstation = "whale" ]; then
 		--freeze-finetune-updates 2000 \
 		--use-fp16 1 \
 		--peak-enc-lr 0.003 \
-		--peak-dec-lr 0.05 \
+		--peak-dec-lr 0.03 \
 		--accum-grads 1 \
 		--encoder-type d2v \
 		--additional-block True \
