271385f0f2da85b1660026c93cdfbdda13879db4
dohe0342 <kimdohe1070@gmail.com>
2022-12-12 22:22:41 +0900

    from local

--- egs/librispeech/ASR/run_v3.sh
+++ egs/librispeech/ASR/run_v3.sh
@@ -1,32 +1,33 @@
 git pull
 
-#export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
-export CUDA_VISIBLE_DEVICES="0,1,2,3"
+workstation="whale"
 
-./pruned_transducer_stateless_d2v_v2/train.py \
-	--wandb true \
-	--input-strategy AudioSamples \
-	--enable-spec-aug False \
-	--multi-optim True \
-	--world-size 4 \
-	--num-epochs 30 \
-	--start-epoch 1 \
-	--full-libri 1 \
-	--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
-	--max-duration 150 \
-	--freeze-finetune-updates 2000 \
-	--use-fp16 1 \
-	--peak-enc-lr 0.001 \
-	--peak-dec-lr 0.05 \
-	--accum-grads 3 \
-	--encoder-type d2v \
-	--additional-block True \
-	--encoder-dim 768 \
-	--decoder-dim 768 \
-	--joiner-dim 768 \
-	--prune-range 10 \
-	--context-size 2 \
-	--ctc-loss-scale 0.2 
+if [ $workstation = "whale" ]; then
+	export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+	./pruned_transducer_stateless_d2v_v2/train.py \
+		--wandb true \
+		--input-strategy AudioSamples \
+		--enable-spec-aug False \
+		--multi-optim True \
+		--world-size 8 \
+		--num-epochs 30 \
+		--start-epoch 1 \
+		--full-libri 1 \
+		--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
+		--max-duration 250 \
+		--freeze-finetune-updates 2000 \
+		--use-fp16 1 \
+		--peak-enc-lr 0.001 \
+		--peak-dec-lr 0.05 \
+		--accum-grads 3 \
+		--encoder-type d2v \
+		--additional-block True \
+		--encoder-dim 768 \
+		--decoder-dim 768 \
+		--joiner-dim 768 \
+		--prune-range 10 \
+		--context-size 2 \
+		--ctc-loss-scale 0.2 
 
 #./pruned_transducer_stateless_d2v/train.py \
 #--initial-lr 0.0003 \
