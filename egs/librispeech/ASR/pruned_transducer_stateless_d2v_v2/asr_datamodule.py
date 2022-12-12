de388dfd92f668522b9ba9035d505be723145921
dohe0342 <kimdohe1070@gmail.com>
2022-12-12 12:23:11 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/asr_datamodule.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/asr_datamodule.py
@@ -97,7 +97,7 @@ class LibriSpeechAsrDataModule:
         group.add_argument(
             "--max-duration",
             type=int,
-            default=200.0,
+            default=250.0,
             help="Maximum pooled recordings duration (seconds) in a "
             "single batch. You can reduce it if it causes CUDA OOM.",
         )
