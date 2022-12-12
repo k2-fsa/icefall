88ce2b3b8a487950d2da43dc417a3e0dec4fbbbe
dohe0342 <kimdohe1070@gmail.com>
2022-12-12 14:28:07 +0900

    from local

--- egs/librispeech/ASR/test.sh
+++ egs/librispeech/ASR/test.sh
@@ -5,8 +5,8 @@ for method in greedy_search modified_beam_search fast_beam_search; do
 	--input-strategy AudioSamples \
 	--enable-spec-aug False \
 	--additional-block True \
-	--epoch 8 \
-    --avg 1 \
+	--epoch 18 \
+    --avg 3 \
 	--exp-dir ./pruned_transducer_stateless_d2v/exp \
     --max-duration 800 \
     --decoding-method $method \
@@ -15,5 +15,5 @@ for method in greedy_search modified_beam_search fast_beam_search; do
     --encoder-dim 768 \
     --decoder-dim 768 \
     --joiner-dim 768 \
-    --use-averaged-model False
+    --use-averaged-model True
 done
