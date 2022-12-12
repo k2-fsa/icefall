c038cea85b8db1ba2c473f3e7871298c594187c6
dohe0342 <kimdohe1070@gmail.com>
2022-12-12 23:57:02 +0900

    from local

--- egs/librispeech/ASR/run_v3.sh
+++ egs/librispeech/ASR/run_v3.sh
@@ -40,12 +40,12 @@ else
 		--start-epoch 1 \
 		--full-libri 1 \
 		--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
-		--max-duration 200 \
+		--max-duration 150 \
 		--freeze-finetune-updates 2000 \
 		--use-fp16 1 \
 		--peak-enc-lr 0.03 \
 		--peak-dec-lr 0.1 \
-		--accum-grads 3 \
+		--accum-grads 5 \
 		--encoder-type d2v \
 		--additional-block True \
 		--encoder-dim 768 \
