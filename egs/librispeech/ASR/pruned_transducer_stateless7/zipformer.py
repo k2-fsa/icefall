22204450dbb87f2a54213c856057f63c35d3efea
Daniel Povey <dpovey@gmail.com>
2022-12-13 18:51:22 +0800

    Make min_abs of AttentionSqueeze smaller, the same as nonlin_attention_module

--- egs/librispeech/ASR/pruned_transducer_stateless7/zipformer.py
+++ egs/librispeech/ASR/pruned_transducer_stateless7/zipformer.py
@@ -1406,7 +1406,7 @@ class AttentionSqueeze(nn.Module):
         self.out_balancer = ActivationBalancer(
             embed_dim, channel_dim=-1,
             min_positive=0.3, max_positive=0.7,
-            min_abs=ScheduledFloat((0.0, 0.002), (8000.0, 0.02), (20000.0, 0.01)),
+            min_abs=ScheduledFloat((0.0, 0.001), (8000.0, 0.01), (20000.0, 0.005)),
         )
 
 
