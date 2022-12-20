286803b7b4f7555a7b70c290200e7f731b314699
dohe0342 <kimdohe1070@gmail.com>
2022-12-20 16:21:13 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -24,17 +24,15 @@ for method in greedy_search modified_beam_search fast_beam_search; do
     --input-strategy AudioSamples \
     --enable-spec-aug False \
     --additional-block True \
-    --epoch 27 \
-    --avg 1 \
+    --model-name epoch-27.pt \
     --exp-dir ./pruned_transducer_stateless_d2v_v2/960h_sweep_v3_388 \
     --max-duration 400 \
     --decoding-method $method \
-    --max-sym-per-frame 1 \
+    --max-sym-per-frame 1 \ 
     --encoder-type d2v \
     --encoder-dim 768 \
     --decoder-dim 768 \
-    --joiner-dim 768 \
-    --use-averaged-model False
+    --joiner-dim 768 
 done
 """
 
