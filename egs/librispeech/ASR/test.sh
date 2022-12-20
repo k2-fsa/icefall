a9dbb2fd2ccea8433d59694cf646f930abf1f81c
dohe0342 <kimdohe1070@gmail.com>
2022-12-20 11:32:06 +0900

    from local

--- egs/librispeech/ASR/test.sh
+++ egs/librispeech/ASR/test.sh
@@ -5,7 +5,7 @@ for method in greedy_search modified_beam_search fast_beam_search; do
 	--input-strategy AudioSamples \
 	--enable-spec-aug False \
 	--additional-block True \
-	--epoch 22 \
+	--epoch 30 \
     --avg 1 \
 	--exp-dir ./pruned_transducer_stateless_d2v_v2/960h_sweep_v3_388 \
     --max-duration 400 \
