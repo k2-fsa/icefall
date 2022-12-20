441020813df8a03b8722c2e249129a83915c7632
dohe0342 <kimdohe1070@gmail.com>
2022-12-20 15:59:45 +0900

    from local

--- egs/librispeech/ASR/run_v3.sh
+++ egs/librispeech/ASR/run_v3.sh
@@ -35,7 +35,7 @@ else
 		--input-strategy AudioSamples \
 		--enable-spec-aug False \
 		--multi-optim True \
-		--start-epoch 24 \
+		--start-epoch 1 \
 		--world-size 4 \
 		--num-epochs 30 \
 		--full-libri 1 \
