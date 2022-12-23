5178627904e8e4ed0a30714456bda395b322eb14
dohe0342 <kimdohe1070@gmail.com>
2022-12-23 17:13:50 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless5/train.py
+++ egs/librispeech/ASR/pruned_transducer_stateless5/train.py
@@ -965,6 +965,8 @@ def run(rank, world_size, args):
 
     logging.info("About to create model")
     model = get_transducer_model(params)
+    logging.info(model)
+    exit()
 
     num_param = sum([p.numel() for p in model.parameters()])
     logging.info(f"Number of model parameters: {num_param}")
