52edf53b599d762b8df675ad9908db266aa7ea69
dohe0342 <kimdohe1070@gmail.com>
2022-12-26 13:35:03 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import argparse
 import logging
 import math
 from dataclasses import dataclass, field
