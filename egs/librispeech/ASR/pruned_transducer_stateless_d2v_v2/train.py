df62af6d16d77f8c64be5d998b4be4d376f72961
dohe0342 <kimdohe1070@gmail.com>
2022-12-26 14:10:05 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train.py
@@ -1524,12 +1524,6 @@ def run_adapter(rank, world_size, args, wb=None):
         else:
             p.requires_grad = False
     
-    for n, p  in model.named_parameters():
-        if p.requires_grad:
-            logging.info(n)
-
-    exit()
-
     optimizer_adapter = ScaledAdam(
             adapter_param,
             lr=0.0001,
