5fff319ede200e674fa6713efe31786659f6f3e4
dohe0342 <kimdohe1070@gmail.com>
2022-12-26 15:25:32 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train.py
@@ -1097,7 +1097,7 @@ def train_one_epoch(
                     f"grad_scale is too small, exiting: {cur_grad_scale}"
                 )
 
-        if params.batch_idx_train > 4000 and loss > 300:
+        if params.batch_idx_train > 4000 and loss > 300 and params.wandb:
             wb.log({"valid/loss": 10000})
             raise RunteimError(
                     f"divergence... exiting: loss={loss}"
