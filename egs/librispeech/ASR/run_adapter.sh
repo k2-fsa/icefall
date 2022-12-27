ac1d5e63fd200d6b286b919aaa1523acf7d8f3c5
dohe0342 <kimdohe1070@gmail.com>
2022-12-27 10:27:45 +0900

    from local

--- egs/librispeech/ASR/run_adapter.sh
+++ egs/librispeech/ASR/run_adapter.sh
@@ -16,17 +16,12 @@ if [ $workstation = "whale" ]; then
 		--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
 		--max-duration 250 \
 		--use-fp16 1 \
-		--peak-enc-lr 0.03 \
-		--peak-dec-lr 0.1 \
-		--accum-grads 1 \
 		--encoder-type d2v \
 		--additional-block True \
 		--encoder-dim 768 \
 		--decoder-dim 768 \
 		--joiner-dim 768 \
-		--prune-range 10 \
-		--context-size 2 \
-		--ctc-loss-scale 0.2 
+		--prune-range 10 
 else
 	export CUDA_VISIBLE_DEVICES="0,1,2,3"
 	./pruned_transducer_stateless_d2v_v2/train.py \
