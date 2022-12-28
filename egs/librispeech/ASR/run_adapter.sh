3b9fde49cd4948051f4cdb37269d2efb40a9754e
dohe0342 <kimdohe1070@gmail.com>
2022-12-28 11:04:00 +0900

    from local

--- egs/librispeech/ASR/run_adapter.sh
+++ egs/librispeech/ASR/run_adapter.sh
@@ -26,7 +26,7 @@ if [ $workstation = "whale" ]; then
 		--prune-range 10 
 else
 	export CUDA_VISIBLE_DEVICES="0,1,2,3"
-	./pruned_transducer_stateless_d2v_v2/train.py \
+	./pruned_transducer_stateless_d2v_v2/train_adapter.py \
 		--add-adapter True \
 		--adapter-lr 0.0001 \
 		--wandb False \
