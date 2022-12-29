88655b90117b476b58082ee463f8ae6bf370ed63
dohe0342 <kimdohe1070@gmail.com>
2022-12-29 11:58:27 +0900

    from local

--- egs/librispeech/ASR/distillation_with_hubert.sh
+++ egs/librispeech/ASR/distillation_with_hubert.sh
@@ -172,7 +172,7 @@ if [ $stage -le 2 ] && [ $stop_stage -ge 2 ]; then
     rm -rf $codebook_download_dir
   fi
   
-  CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+  export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
   ./pruned_transducer_stateless6/extract_codebook_index.py \
     --full-libri $full_libri \
     --exp-dir $exp_dir \
