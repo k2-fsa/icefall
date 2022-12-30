25f7095fd9bc6df515844feb98fd34c02d2ae534
dohe0342 <kimdohe1070@gmail.com>
2022-12-30 13:44:43 +0900

    from local

--- egs/librispeech/ASR/run_adapter.sh
+++ egs/librispeech/ASR/run_adapter.sh
@@ -7,23 +7,45 @@ if [ $workstation = "whale" ]; then
 	./pruned_transducer_stateless_d2v_v2/train_adapter.py \
 		--add-adapter True \
 		--adapter-lr 0.001 \
-		--gender female
+		--gender male \
 		--wandb False \
 		--input-strategy AudioSamples \
 		--enable-spec-aug False \
 		--multi-optim False \
-		--world-size 8 \
+		--world-size 4 \
 		--num-epochs 10 \
 		--full-libri 1 \
 		--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
-		--max-duration 200 \
-		--use-fp16 0 \
-		--encoder-type d2v \
-		--additional-block True \
+		--max-duration 150 \
 		--encoder-dim 768 \
 		--decoder-dim 768 \
 		--joiner-dim 768 \
+		--use-fp16 0 \
+		--accum-grads 4 \
+		--encoder-type d2v \
+		--additional-block True \
 		--prune-range 10 
+
+#	./pruned_transducer_stateless_d2v_v2/train_adapter.py \
+#		--add-adapter True \
+#		--adapter-lr 0.001 \
+#		--gender female
+#		--wandb False \
+#		--input-strategy AudioSamples \
+#		--enable-spec-aug False \
+#		--multi-optim False \
+#		--world-size 8 \
+#		--num-epochs 10 \
+#		--full-libri 1 \
+#		--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
+#		--max-duration 200 \
+#		--use-fp16 0 \
+#		--encoder-type d2v \
+#		--additional-block True \
+#		--encoder-dim 768 \
+#		--decoder-dim 768 \
+#		--joiner-dim 768 \
+#		--prune-range 10 
 else
 	export CUDA_VISIBLE_DEVICES="0,1,2,3"
 	./pruned_transducer_stateless_d2v_v2/train_adapter.py \
