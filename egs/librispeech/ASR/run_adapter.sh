c7a0095f0551bb6d7b991cedace8c1c6636431b6
dohe0342 <kimdohe1070@gmail.com>
2022-12-30 13:45:04 +0900

    from local

--- egs/librispeech/ASR/run_adapter.sh
+++ egs/librispeech/ASR/run_adapter.sh
@@ -12,16 +12,16 @@ if [ $workstation = "whale" ]; then
 		--input-strategy AudioSamples \
 		--enable-spec-aug False \
 		--multi-optim False \
-		--world-size 4 \
+		--world-size 8 \
 		--num-epochs 10 \
 		--full-libri 1 \
 		--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
-		--max-duration 150 \
+		--max-duration 250 \
 		--encoder-dim 768 \
 		--decoder-dim 768 \
 		--joiner-dim 768 \
 		--use-fp16 0 \
-		--accum-grads 4 \
+		--accum-grads 1 \
 		--encoder-type d2v \
 		--additional-block True \
 		--prune-range 10 
