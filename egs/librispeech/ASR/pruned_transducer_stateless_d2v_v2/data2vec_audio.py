9b16457fdc64c46a7cab9f4bc4beb8729b2c3afc
dohe0342 <kimdohe1070@gmail.com>
2022-12-30 11:56:40 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
@@ -41,6 +41,7 @@ class TransformerEncoderAdapter(TransformerEncoder):
         for p in self.adapters.parameters():
             #p.data = nn.Parameter(torch.zeros(p.size()).to('cuda'))
             p.data = nn.Parameter(torch.randn(p.size()).to('cuda')/10000.)
+            print(p)
 
     def forward(self, x, padding_mask=None, layer=None, tgt_layer=None):
         x, layer_results = self.extract_features_with_adapter(
