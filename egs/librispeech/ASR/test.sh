df5956c4da0f1aa3893bad91e0dd9cfcd432801c
dohe0342 <kimdohe1070@gmail.com>
2022-12-30 11:10:25 +0900

    from local

--- egs/librispeech/ASR/test.sh
+++ egs/librispeech/ASR/test.sh
@@ -5,7 +5,7 @@ for method in greedy_search modified_beam_search fast_beam_search; do
 	--input-strategy AudioSamples \
 	--enable-spec-aug False \
 	--additional-block True \
-	--model-name epoch-10.pt \
+	--model-name d2v-base-T.pt \
 	--exp-dir ./pruned_transducer_stateless_d2v_v2/d2v-T-adapter_male \
     --max-duration 400 \
     --decoding-method $method \
