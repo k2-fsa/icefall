e29b45acb17d9f6872086c6ddaf8a6135aeebeb9
dohe0342 <kimdohe1070@gmail.com>
2022-12-30 11:17:13 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/checkpoint.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/checkpoint.py
@@ -122,6 +122,7 @@ def load_checkpoint(
     """
     TODO: document it
     """
+    print(model)
     logging.info(f"Loading checkpoint from {filename}")
     checkpoint = torch.load(filename, map_location="cpu")
 
