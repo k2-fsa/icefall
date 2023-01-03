99d0612de04fdd916d43f4d19d3249e9cd08cc54
dohe0342 <kimdohe1070@gmail.com>
2023-01-03 19:30:51 +0900

    from local

--- egs/librispeech/ASR/test.sh
+++ egs/librispeech/ASR/test.sh
@@ -7,7 +7,7 @@ for method in greedy_search modified_beam_search fast_beam_search; do
 	--additional-block True \
 	--model-name epoch-2.pt \
 	--exp-dir ./pruned_transducer_stateless_d2v_v2/conv_adapter \
-    --max-duration 150 \
+    --max-duration 400 \
     --decoding-method $method \
     --max-sym-per-frame 1 \
 	--encoder-type d2v \
