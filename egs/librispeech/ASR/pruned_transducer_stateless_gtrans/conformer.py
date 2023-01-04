018c17873e56c7a52a2cb0f1778c99d12dbba426
dohe0342 <kimdohe1070@gmail.com>
2023-01-04 19:04:56 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_gtrans/conformer.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_gtrans/conformer.py
@@ -203,15 +203,13 @@ class Conformer(EncoderInterface):
 
         x = x.permute(1, 0, 2)  # (T, N, C) ->(N, T, C)
         
-        '''
         layer_output = [x.permute(1, 0, 2) for x in layer_output]
-        x = self.layer_norm(1/4*(self.sigmoid(self.alpha[0])*layer_output[5] + \
+        x = self.layer_norm(1/4*(self.sigmoid(self.alpha[0])*layer_output[2] + \
                                  self.sigmoid(self.alpha[1])*layer_output[5] + \
-                                 self.sigmoid(self.alpha[2])*layer_output[5] + \
-                                 self.sigmoid(self.alpha[3])*layer_output[5]
+                                 self.sigmoid(self.alpha[2])*layer_output[8] + \
+                                 self.sigmoid(self.alpha[3])*layer_output[11]
                                 )
                             )
-        '''
 
         return x, lengths
 
