a98a6e8d6a8a164e54d96cc42e83bfa74dd6815a
dohe0342 <kimdohe1070@gmail.com>
2023-01-08 19:19:42 +0900

    from local

--- egs/librispeech/ASR/gtrans.sh
+++ egs/librispeech/ASR/gtrans.sh
@@ -9,7 +9,7 @@ do
 				--start-epoch 1 \
 				--full-libri 1 \
 				--exp-dir pruned_transducer_stateless_gtrans/nocombine_layer12_group$group_num	\
-				--max-duration 700 \
+				--max-duration 750 \
 				--use-fp16 1 \
 				--num-encoder-layers 12 \
 				--group-num $group_num \
