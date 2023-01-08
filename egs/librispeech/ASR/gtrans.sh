0b6c728fa46dc3b4905498b6da70fde34fb6ac75
dohe0342 <kimdohe1070@gmail.com>
2023-01-08 18:36:07 +0900

    from local

---
+++ egs/librispeech/ASR/gtrans.sh
@@ -0,0 +1,15 @@
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+./pruned_transducer_stateless_gtrans/train.py \
+			--world-size 8 \
+			--num-epochs 30 \
+			--start-epoch 1 \
+			--full-libri 1 \
+			--exp-dir pruned_transducer_stateless_gtrans/layer12_group6	\
+			--max-duration 600 \
+			--use-fp16 1 \
+			--num-encoder-layers 12 \
+			--dim-feedforward 2048 \
+			--nhead 8 \
+			--encoder-dim 512 \
+			--decoder-dim 512 \
+			--joiner-dim 512
