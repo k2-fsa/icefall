e0cd9cdebff2b85296cd7b77475fc325a19f2159
dohe0342 <kimdohe1070@gmail.com>
2023-01-08 18:53:47 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_gtrans/conformer.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_gtrans/conformer.py
@@ -126,7 +126,7 @@ class Conformer(EncoderInterface):
         self._init_state: List[torch.Tensor] = [torch.empty(0)]
 
         self.group_num = group_num
-        self.group_layer_num = int(self.encoder_layers // self.group_size)
+        self.group_layer_num = int(self.encoder_layers // self.group_num)
         self.alpha = nn.Parameter(torch.rand(self.group_size))
         self.sigmoid = nn.Sigmoid()
         self.layer_norm = nn.LayerNorm(d_model)
