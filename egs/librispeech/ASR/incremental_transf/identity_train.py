3c8f1b10f3b1290bb91f3f9629010a0677595bd8
dohe0342 <kimdohe1070@gmail.com>
2023-01-08 17:53:34 +0900

    from local

--- egs/librispeech/ASR/incremental_transf/identity_train.py
+++ egs/librispeech/ASR/incremental_transf/identity_train.py
@@ -965,6 +965,15 @@ def run(rank, world_size, args):
 
     logging.info("About to create model")
     model = get_transducer_model(params)
+    
+    pre_trained_model = torch.load('/home/work/workspace/icefall/egs/librispeech/pruned_transducer_stateless5/exp-B/epoch-30.pt')
+    pre_trained_model = pre_trained_mdoel['model']
+
+    for n, p in model.named_parameters():
+        if 'layer' not in n:
+            p.data = pre_trained_model[n]
+        else:
+            print(n)
 
     num_param = sum([p.numel() for p in model.parameters()])
     logging.info(f"Number of model parameters: {num_param}")
