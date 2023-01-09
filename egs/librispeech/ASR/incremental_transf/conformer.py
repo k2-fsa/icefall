dc477d9711c2c3881760bf22dc1f67946beacfa8
dohe0342 <kimdohe1070@gmail.com>
2023-01-09 19:28:37 +0900

    from local

--- egs/librispeech/ASR/incremental_transf/conformer.py
+++ egs/librispeech/ASR/incremental_transf/conformer.py
@@ -198,7 +198,10 @@ class Conformer(EncoderInterface):
         x = x.permute(1, 0, 2)  # (T, N, C) ->(N, T, C)
         layer_outputs = [x.permute(1, 0, 2) for x in layer_outputs]
 
-        return x, lengths
+        if get_layer_output:
+            return x, lengths, layer_outputs
+        else:
+            return x, lengths
 
     @torch.jit.export
     def get_init_state(
