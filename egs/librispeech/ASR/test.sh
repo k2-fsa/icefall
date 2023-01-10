4e0a4c003254988641225ab591a26d853bfcc39c
dohe0342 <kimdohe1070@gmail.com>
2023-01-10 11:10:15 +0900

    from local

--- egs/librispeech/ASR/test.sh
+++ egs/librispeech/ASR/test.sh
@@ -7,7 +7,7 @@ do
 	--input-strategy AudioSamples \
 	--enable-spec-aug False \
 	--additional-block True \
-	--model-name epoch-4.pt \
+	--model-name epoch-$i.pt \
 	--exp-dir ./pruned_transducer_stateless_d2v_v2/male_hpo6 \
     --max-duration 600 \
     --decoding-method $method \
