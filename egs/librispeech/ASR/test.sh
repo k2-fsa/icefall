2f326a107674bcbc0134b3fa9cc8c70f32cfeb21
dohe0342 <kimdohe1070@gmail.com>
2023-01-10 11:10:10 +0900

    from local

--- egs/librispeech/ASR/test.sh
+++ egs/librispeech/ASR/test.sh
@@ -1,6 +1,8 @@
 git pull
 
-for method in greedy_search modified_beam_search fast_beam_search; do
+#for method in greedy_search modified_beam_search fast_beam_search; do
+for i in {1..6}
+do
   ./pruned_transducer_stateless_d2v_v2/decode.py \
 	--input-strategy AudioSamples \
 	--enable-spec-aug False \
