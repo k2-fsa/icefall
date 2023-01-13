42e887e36b3ed14bb91f2a1b984dd5a79e407fd9
Teo <teouuenshen@gmail.com>
2023-01-14 00:03:44 +0900

    save cuts in fbank_dir

--- egs/csj/ASR/local/add_transcript_mode.py
+++ egs/csj/ASR/local/add_transcript_mode.py
@@ -2,10 +2,10 @@ import argparse
 import logging
 from configparser import ConfigParser
 from pathlib import Path
+from typing import List
 
 from lhotse import CutSet, SupervisionSet
 from lhotse.recipes.csj import CSJSDBParser
-from lhotse.serialization import Serializable
 
 ARGPARSE_DESCRIPTION = """
 This script adds transcript modes to an existing CutSet or SupervisionSet.
@@ -20,12 +20,34 @@ def get_args():
         "-f",
         "--fbank-dir",
         type=Path,
-        help="Path to fbank dir where cut manifests are stored.",
+        help="Path to directory where manifests are stored.",
     )
     parser.add_argument(
-        "-c", "--config", type=Path, help="Path to config file for transcript parsing."
+        "-c",
+        "--config",
+        type=Path,
+        nargs="+",
+        help="Path to config file for transcript parsing.",
     )
     return parser.parse_args()
+
+
+def get_CSJParsers(config_files: List[Path]) -> List[CSJSDBParser]:
+    parsers = []
+    for config_file in config_files:
+        config = ConfigParser()
+        config.optionxform = str
+        assert config.read(config_file), f"{config_file} could not be found."
+        decisions = {}
+        for k, v in config["DECISIONS"].items():
+            try:
+                decisions[k] = int(v)
+            except ValueError:
+                decisions[k] = v
+        parsers.append(
+            (config["CONSTANTS"].get("MODE"), CSJSDBParser(decisions=decisions))
+        )
+    return parsers
 
 
 def main():
@@ -34,6 +56,7 @@ def main():
         format=("%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"),
         level=logging.INFO,
     )
+    parsers = get_CSJParsers(args.config)
     config = ConfigParser()
     config.optionxform = str
     assert config.read(args.config), args.config
@@ -43,41 +66,27 @@ def main():
             decisions[k] = int(v)
         except ValueError:
             decisions[k] = v
-    parser = CSJSDBParser(decisions=decisions)
-    name = config["CONSTANTS"].get("MODE")
-    logging.info(f"Parsing in {name} mode.")
+
+    logging.info(f"Adding {', '.join(x[0] for x in parsers)} transcript mode.")
 
     manifests = args.fbank_dir.glob("csj_cuts_*.jsonl.gz")
-    if manifests:
-        for manifest in manifests:
-            results = []
-            logging.info(f"Adding transcript mode {name} to {manifest.name} now.")
-            cutset = CutSet.from_file(manifest)
-            for cut in cutset:
+    assert manifests, f"No cuts to be found in {args.fbank_dir}"
+
+    for manifest in manifests:
+        results = []
+        logging.info(f"Adding transcript modes to {manifest.name} now.")
+        cutset = CutSet.from_file(manifest)
+        for cut in cutset:
+            for name, parser in parsers:
                 cut.supervisions[0].custom[name] = parser.parse(
                     cut.supervisions[0].custom["raw"]
                 )
-                cut.supervisions[0].text = ""
-                results.append(cut)
-            results = CutSet.from_items(results)
-            res_file = manifest.as_posix()
-            manifest.replace(manifest.parent / ("bak." + manifest.name))
-            results.to_file(res_file)
-    else:
-        manifests = args.fbank_dir.glob("csj_supervisions_*.jsonl.gz")
-        assert manifests, f"No supervisions or cuts can be found in {args.fbank_dir}"
-        for manifest in manifests:
-            results = []
-            logging.info(f"Adding transcript mode {name} to {manifest.name} now.")
-            spset = SupervisionSet.from_file(manifest)
-            for sp in spset:
-                sp.custom[name] = parser.parse(sp.custom["raw"])
-                sp.text = ""
-                results.append(sp)
-            results = SupervisionSet.from_items(results)
-            res_file = manifest.as_posix()
-            manifest.replace(manifest.parent / ("bak." + manifest.name))
-            results.to_file(res_file)
+            cut.supervisions[0].text = ""
+            results.append(cut)
+        results = CutSet.from_items(results)
+        res_file = manifest.as_posix()
+        manifest.replace(manifest.parent / ("bak." + manifest.name))
+        results.to_file(res_file)
 
 
 if __name__ == "__main__":
