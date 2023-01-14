70b04d0758f56c96f0aefb786db507997ddaaac2
dohe0342 <kimdohe1070@gmail.com>
2023-01-14 10:57:24 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_gtrans/train.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_gtrans/train.py
@@ -340,7 +340,7 @@ def get_parser():
     parser.add_argument(
         "--save-every-n",
         type=int,
-        default=4000000,
+        default=4000,
         help="""Save checkpoint after processing this number of batches"
         periodically. We save checkpoint to exp-dir/ whenever
         params.batch_idx_train % save_every_n == 0. The checkpoint filename
