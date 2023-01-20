ca8ee77d559c46bc73bbea0fc339bb728f2a3595
dohe0342 <kimdohe1070@gmail.com>
2023-01-20 13:58:32 +0900

    from local

--- egs/librispeech/ASR/conformer_ctc2/train.py
+++ egs/librispeech/ASR/conformer_ctc2/train.py
@@ -324,7 +324,7 @@ def get_params() -> AttributeDict:
             "best_train_epoch": -1,
             "best_valid_epoch": -1,
             "batch_idx_train": 0,
-            "log_interval": 1,
+            "log_interval": 50,
             "reset_interval": 200,
             "valid_interval": 3000,  # For the 100h subset, use 800
             # parameters for conformer
