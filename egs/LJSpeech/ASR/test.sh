d2808f8c2315f5f960d6fac26c8ee602a408b5cf
dohe0342 <kimdohe1070@gmail.com>
2023-01-24 14:58:38 +0900

    from local

--- egs/LJSpeech/ASR/test.sh
+++ egs/LJSpeech/ASR/test.sh
@@ -1,6 +1,6 @@
 git pull
 
-for method in greedy_search modified_beam_search fast_beam_search; do
+for method in greedy_search modified_beam_search fast_beam_search
 do
   ./pruned_transducer_stateless_d2v_v2/decode.py \
 	--input-strategy AudioSamples \
