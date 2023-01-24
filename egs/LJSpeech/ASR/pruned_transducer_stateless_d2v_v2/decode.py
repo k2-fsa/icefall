237d97d4517d16d48d2898ac5e3916c2c8735a28
dohe0342 <kimdohe1070@gmail.com>
2023-01-24 15:06:38 +0900

    from local

--- egs/LJSpeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/LJSpeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -59,8 +59,8 @@ from beam_search import (
     greedy_search_batch,
     modified_beam_search,
 )
-#from train import add_model_arguments, add_rep_arguments, get_params, get_transducer_model
-from train_adapter import add_model_arguments, add_rep_arguments, get_params, get_transducer_model
+from train import add_model_arguments, add_rep_arguments, get_params, get_transducer_model
+#from train_adapter import add_model_arguments, add_rep_arguments, get_params, get_transducer_model
 
 from icefall.checkpoint import (
     average_checkpoints,
