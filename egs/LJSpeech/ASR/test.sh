4f43c0d8de62e1ccd24e2a351fb803db16be4a32
dohe0342 <kimdohe1070@gmail.com>
2023-01-24 17:39:30 +0900

    from local

--- egs/LJSpeech/ASR/test.sh
+++ egs/LJSpeech/ASR/test.sh
@@ -8,7 +8,7 @@ do
 	--additional-block True \
 	--model-name epoch-30.pt \
 	--exp-dir ./pruned_transducer_stateless_d2v_v2/full_ft_origin_param \
-    --max-duration 100 \
+    --max-duration 600 \
     --decoding-method $method \
     --max-sym-per-frame 1 \
 	--encoder-type d2v \
