a192cba20b0abdddaad69967029afcd9e6b1a725
dohe0342 <kimdohe1070@gmail.com>
2023-01-24 17:39:13 +0900

    from local

--- egs/LJSpeech/ASR/test.sh
+++ egs/LJSpeech/ASR/test.sh
@@ -6,7 +6,7 @@ do
 	--input-strategy AudioSamples \
 	--enable-spec-aug False \
 	--additional-block True \
-	--model-name epoch-13.pt \
+	--model-name epoch-30.pt \
 	--exp-dir ./pruned_transducer_stateless_d2v_v2/full_ft_origin_param \
     --max-duration 100 \
     --decoding-method $method \
