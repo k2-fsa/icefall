4d5b9e771357cc82aea62e8e3ecaeeee7a6764c3
dohe0342 <kimdohe1070@gmail.com>
2023-01-24 13:55:25 +0900

    from local

--- egs/LJSpeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
+++ egs/LJSpeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
@@ -1576,7 +1576,7 @@ def run_adapter(rank, world_size, args, wb=None):
             train_cuts += librispeech.train_clean_360_cuts(option=params.gender)
             train_cuts += librispeech.train_other_500_cuts(option=params.gender)
         '''
-        train_cuts = LJSpeechAsrDataModule 
+        train_cuts = LJSpeechAsrDataModule.train_cuts()
     def remove_short_and_long_utt(c: Cut):
         return 1.0 <= c.duration <= 20.0
 
