5f689ccdd2e609d8116ef2519cf7fc6c94cfc8de
dohe0342 <kimdohe1070@gmail.com>
2023-01-25 17:21:12 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
@@ -833,6 +833,7 @@ def compute_loss(
     warm_step = params.warm_step
 
     texts = batch["supervisions"]["text"]
+    print('hi~~~')
     print(texts)
     #texts = batch["greedy pseudo text"]
     token_ids = sp.encode(texts, out_type=int)
