7511cd229c088c2fc2e65ed4bde08a004f7aff37
dohe0342 <kimdohe1070@gmail.com>
2023-01-25 16:33:56 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -808,7 +808,8 @@ def main():
 
             pseudo = f'"psudo text": "{res_dict[utt_id]}",'
             splited.insert(text_idx-1, pseudo)
-            print(' '.join(splited))
+            print(text_idx)
+            #print(' '.join(splited))
 
     '''
     for test_set, test_dl in zip(test_sets, test_dl):
