8c247366ce39f407c1c949ac71e024ad8fc82ae4
dohe0342 <kimdohe1070@gmail.com>
2023-01-26 10:40:25 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
@@ -1579,7 +1579,7 @@ def run_adapter(rank, world_size, args, wb=None):
             train_cuts += librispeech.train_other_500_cuts(option=params.gender)
     '''
 
-    train_cuts = librispeech.test_clean_user(option='low')
+    train_cuts = librispeech.test_clean_user(option='big')
 
     def remove_short_and_long_utt(c: Cut):
         return 1.0 <= c.duration <= 20.0
