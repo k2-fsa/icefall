8abf9deede5228a499c1aa0185acd70d62b7b1b3
dohe0342 <kimdohe1070@gmail.com>
2023-01-27 18:09:13 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -778,9 +778,10 @@ def main():
     option = 'big'
     #option = '4446'
     #test_clean_cuts = librispeech.vox_cuts(option=option)
-    test_other_cuts = librispeech.test_other_user(option=option)
+    test_clean_cuts = librispeech.test_clean_user(option=option)
+    #test_other_cuts = librispeech.test_other_user(option=option)
 
-    #test_clean_dl = librispeech.test_dataloaders(test_clean_cuts)
+    test_clean_dl = librispeech.test_dataloaders(test_clean_cuts)
     #test_other_dl = librispeech.test_dataloaders(test_other_cuts)
 
     test_sets = [f"test-clean_sampling"]
