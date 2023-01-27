9588029f0c75ed3ca5563e667ef4b806b6492cd1
dohe0342 <kimdohe1070@gmail.com>
2023-01-28 02:48:26 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless5/train.py
+++ egs/librispeech/ASR/pruned_transducer_stateless5/train.py
@@ -983,7 +983,7 @@ def run(rank, world_size, args):
     model.to(device)
     if world_size > 1:
         logging.info("Using DDP")
-        model = DDP(model, device_ids=[rank])
+        model = DDP(model, device_ids=[rank], find_unused_parameters=True)
 
     optimizer = Eve(model.parameters(), lr=params.initial_lr)
 
