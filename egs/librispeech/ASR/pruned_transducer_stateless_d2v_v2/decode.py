ac63636472f785cff5ff1d0948cd9422d8190718
dohe0342 <kimdohe1070@gmail.com>
2023-01-27 17:34:07 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -785,7 +785,6 @@ def main():
     test_sets = [f"test-clean_sampling", f"test-other_sampling"]
     test_dl = [test_clean_dl, test_other_dl]
     
-    '''
     for test_set, test_dl in zip(test_sets, test_dl):
         results_dict = decode_dataset(
             dl=test_dl,
@@ -819,7 +818,6 @@ def main():
             new_line += '\n'
 
             new_jsons.write(new_line)
-    '''
 
     '''
     for test_set, test_dl in zip(test_sets, test_dl):
