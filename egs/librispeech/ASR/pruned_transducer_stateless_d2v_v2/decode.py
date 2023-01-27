ac71fbc78f3dad0d394597663c618364160b04f5
dohe0342 <kimdohe1070@gmail.com>
2023-01-27 17:39:02 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -797,8 +797,8 @@ def main():
             decoding_graph=decoding_graph,
         )
         results = results_dict['greedy_search']
-        jsons = open(f"{params.manifest_dir}/userlibri/{test_set}/{option}.jsonl", 'r').readlines()
-        new_jsons = open(f"{params.manifest_dir}/userlibri/{test_set}/{option}_p.jsonl", 'w')
+        #jsons = open(f"{params.manifest_dir}/userlibri/{test_set}/{option}.jsonl", 'r').readlines()
+        #new_jsons = open(f"{params.manifest_dir}/userlibri/{test_set}/{option}_p.jsonl", 'w')
 
         res_dict = {}
         for res in results:
