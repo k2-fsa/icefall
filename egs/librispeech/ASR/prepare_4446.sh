4d72c0b1f1649254ad20aef76bed9bd5d62e195c
dohe0342 <kimdohe1070@gmail.com>
2023-01-27 16:36:42 +0900

    from local

--- egs/librispeech/ASR/prepare_4446.sh
+++ egs/librispeech/ASR/prepare_4446.sh
@@ -76,7 +76,7 @@ if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
   # |-- metadata.csv
 
   # to $dl_dir/LJSpeech
-  if [ ! -e $dl_dir/user/.user.done ]; then
+  if [ ! -e $dl_dir/vox/.user.done ]; then
     for dset in "4446"; do
       log "Resampling user/$dset set"
       file_list=`ls $dl_dir/user/$dset/`
