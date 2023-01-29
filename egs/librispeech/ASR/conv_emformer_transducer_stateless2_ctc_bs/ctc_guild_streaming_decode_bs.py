c6c791f2ec1fc1d27953718de128fd765c53e129
yifanyang <yifanyeung@yifanyangs-MacBook-Pro.local>
2023-01-29 10:26:01 +0800

    fix for style check

--- egs/librispeech/ASR/conv_emformer_transducer_stateless2_ctc_bs/ctc_guild_streaming_decode_bs.py
+++ egs/librispeech/ASR/conv_emformer_transducer_stateless2_ctc_bs/ctc_guild_streaming_decode_bs.py
@@ -292,10 +292,8 @@ def greedy_search(
     encoder_out = model.joiner.encoder_proj(fake_packed_encoder_out.data)
 
     decoder_input = torch.tensor(
-        [
-            streams[sorted_indices[i]].hyp[-context_size:]
-            for i in range(streams_length)
-        ],
+        [streams[sorted_indices[i]].hyp[-context_size:] for i in
+range(streams_length)],
         device=device,
         dtype=torch.int64,
     )
@@ -331,7 +329,10 @@ def greedy_search(
         if emitted:
             # update decoder output
             decoder_input = torch.tensor(
-                [streams[sorted_indices[i]].hyp[-context_size:] for i in range(streams_length)],
+                [
+                    streams[sorted_indices[i]].hyp[-context_size:]
+                    for i in range(streams_length)
+                ],
                 device=device,
                 dtype=torch.int64,
             )
