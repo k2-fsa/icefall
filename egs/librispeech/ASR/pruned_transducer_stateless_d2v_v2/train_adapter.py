03c60904f1a85d0f658d8ea12176fc954a84a6f9
dohe0342 <kimdohe1070@gmail.com>
2023-01-30 16:39:44 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
@@ -1589,7 +1589,7 @@ def run_adapter(rank, world_size, args, wb=None):
 
     #train_cuts = librispeech.train_clean_10_cuts(option='male')
     #train_cuts = librispeech.test_clean_user(option='big')
-    train_cuts = librispeech.vox_cuts(option='4446')
+    train_cuts = librispeech.vox_cuts(option='6938')
     
     with open('/workspace/icefall/egs/librispeech/ASR/4446.pl', 'r') as pl_texts:
         pl_texts = pl_texts.readlines()
