02c3c9b8f3a5aea58001b205c158408a1abafe4c
dohe0342 <kimdohe1070@gmail.com>
2023-01-30 16:59:44 +0900

    from local

--- egs/librispeech/ASR/test.sh
+++ egs/librispeech/ASR/test.sh
@@ -1,21 +1,23 @@
 git pull
 
 for i in 10 20 30 40 50
-for method in greedy_search #modified_beam_search fast_beam_search
 do
-  ./pruned_transducer_stateless_d2v_v2/decode.py \
-	--input-strategy AudioSamples \
-	--enable-spec-aug False \
-	--additional-block True \
-	--model-name epoch-10.pt \
-	--exp-dir ./pruned_transducer_stateless_d2v_v2/6938_test \
-    --max-duration 600 \
-    --decoding-method $method \
-    --max-sym-per-frame 1 \
-	--encoder-type d2v \
-    --encoder-dim 768 \
-    --decoder-dim 768 \
-    --joiner-dim 768 \
-	--avg 1 \
-	--use-averaged-model True
+	for method in greedy_search #modified_beam_search fast_beam_search
+	do
+	  ./pruned_transducer_stateless_d2v_v2/decode.py \
+		--input-strategy AudioSamples \
+		--enable-spec-aug False \
+		--additional-block True \
+		--model-name epoch-10.pt \
+		--exp-dir ./pruned_transducer_stateless_d2v_v2/6938_test \
+		--max-duration 600 \
+		--decoding-method $method \
+		--max-sym-per-frame 1 \
+		--encoder-type d2v \
+		--encoder-dim 768 \
+		--decoder-dim 768 \
+		--joiner-dim 768 \
+		--avg 1 \
+		--use-averaged-model True
+	done
 done
