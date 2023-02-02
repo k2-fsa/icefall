89b541e8002a41b889e429d5c5874bdd55490b77
dohe0342 <kimdohe1070@gmail.com>
2023-02-02 11:10:07 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_gtrans/conformer_randomcombine.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_gtrans/conformer_randomcombine.py
@@ -221,9 +221,9 @@ class Conformer(EncoderInterface):
                 x += self.sigmoid(alpha) * layer_outputs[(enum+1)*self.group_layer_num-1]
             x = self.layer_norm(x/self.group_num)
             '''
-            x = self.sigmoid(self.alpha[-1]) * layer_outputs[17]
-            #x += self.sigmoid(self.alpha[-2]) * layer_outputs[14]
-            #x += self.sigmoid(self.alpha[-3]) * layer_outputs[11]
+            #x = self.sigmoid(self.alpha[-1]) * layer_outputs[17]
+            x = self.sigmoid(self.alpha[-2]) * layer_outputs[14]
+            x += self.sigmoid(self.alpha[-3]) * layer_outputs[11]
             x += self.sigmoid(self.alpha[-4]) * layer_outputs[8]
             x += self.sigmoid(self.alpha[-5]) * layer_outputs[5]
             x += self.sigmoid(self.alpha[-6]) * layer_outputs[2]
