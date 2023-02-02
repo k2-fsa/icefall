3858fccb330b75a457a9d53e3494eaf06b00cd6d
dohe0342 <kimdohe1070@gmail.com>
2023-02-02 23:01:24 +0900

    from local

--- egs/aishell/ASR/transformer_ctc/train.py
+++ egs/aishell/ASR/transformer_ctc/train.py
@@ -215,7 +215,8 @@ def get_params() -> AttributeDict:
             # parameters for Noam
             "weight_decay": 1e-5,
             "lr_factor": 5.0,
-            "warm_step": 36000,
+            #"warm_step": 36000,
+            "warm_step": 200,
             "env_info": get_env_info(),
         }
     )
