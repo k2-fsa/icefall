26f345713df0931d5f21880922487f8f7a13473a
dohe0342 <kimdohe1070@gmail.com>
2023-02-02 15:11:24 +0900

    from local

--- egs/aishell/ASR/conformer_ctc/train.py
+++ egs/aishell/ASR/conformer_ctc/train.py
@@ -442,13 +442,13 @@ def compute_validation_loss(
         tot_loss.reduce(loss.device)
 
     loss_value = tot_loss["loss"] / tot_loss["frames"]
-    if params.cur_epoch < 3:
+    if params.cur_epoch < 2:
         params.best_valid_losses[params.cur_epoch] = loss_value
 
     #if loss_value < params.best_valid_loss:
     #    params.best_valid_epoch = params.cur_epoch
     #    params.best_valid_loss = loss_value
-    if params.cur_epoch >= 3 and loss_value < max(params.best_valid_losses.values()):
+    if params.cur_epoch >= 2 and loss_value < max(params.best_valid_losses.values()):
         for k, v in params.best_valid_losses.items():
             if v == max(params.best_valid_losses.values()):
                 remove_filename = params.exp_dir / f"best-valid-loss_{k}.pt"
