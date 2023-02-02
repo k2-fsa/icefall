74b7de5f35c2d3a2e9c1b6637470aca3cf01521f
dohe0342 <kimdohe1070@gmail.com>
2023-02-03 01:24:29 +0900

    from local

--- egs/aishell/ASR/transformer_ctc/train.py
+++ egs/aishell/ASR/transformer_ctc/train.py
@@ -715,7 +715,7 @@ def main():
     else: args.exp_dir = Path(args.exp_dir)
     args.lang_dir = Path(args.lang_dir)
 
-    if args.wandb: wb = wandb.init(project="d2v-T", entity="dohe0342", config=vars(args))
+    if args.wandb: wb = wandb.init(project="aishell", entity="dohe0342", config=vars(args))
     else: wb = None
 
     world_size = args.world_size
