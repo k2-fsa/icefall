a1e31999d2160eeae1f465f42845ddd51440d3a1
dohe0342 <kimdohe1070@gmail.com>
2023-02-02 14:56:37 +0900

    from local

--- egs/aishell/ASR/conformer_ctc/train.py
+++ egs/aishell/ASR/conformer_ctc/train.py
@@ -440,8 +440,6 @@ def compute_validation_loss(
 
     loss_value = tot_loss["loss"] / tot_loss["frames"]
     if params.cur_epoch < 10:
-        #params.best_valid_losses.append(loss_value)
-        #params.best_valid_epochs.append(params.cur_epoch)
         params.best_valid_losses[params.cur_epoch] = loss_value
 
     #if loss_value < params.best_valid_loss:
