7f6bb0e05129b69cca91d800c2fb99dc31b12409
dohe0342 <kimdohe1070@gmail.com>
2023-02-02 14:50:39 +0900

    from local

--- egs/aishell/ASR/conformer_ctc/train.py
+++ egs/aishell/ASR/conformer_ctc/train.py
@@ -193,6 +193,7 @@ def get_params() -> AttributeDict:
         {
             "best_train_loss": float("inf"),
             "best_valid_loss": float("inf"),
+            "best_valid_losses": [float("inf") for i in range(10)],
             "best_train_epoch": -1,
             "best_valid_epoch": -1,
             "batch_idx_train": 0,
