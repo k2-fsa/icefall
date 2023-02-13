3e4c93cf915a7329e4c51a3846461dbe948a132a
dohe0342 <kimdohe1070@gmail.com>
2023-02-14 01:23:01 +0900

    from local

--- egs/tedlium2/ASR/conformer_ctc2/decode.py
+++ egs/tedlium2/ASR/conformer_ctc2/decode.py
@@ -28,7 +28,7 @@ import k2
 import sentencepiece as spm
 import torch
 import torch.nn as nn
-from asr_datamodule import LibriSpeechAsrDataModule
+from asr_datamodule import TedAsrDataModule
 from conformer import Conformer
 
 from icefall.bpe_graph_compiler import BpeCtcTrainingGraphCompiler
@@ -731,7 +731,7 @@ def save_results(
 @torch.no_grad()
 def main():
     parser = get_parser()
-    LibriSpeechAsrDataModule.add_arguments(parser)
+    TedAsrDataModule.add_arguments(parser)
     args = parser.parse_args()
     args.exp_dir = Path(args.exp_dir)
     args.lang_dir = Path(args.lang_dir)
@@ -960,13 +960,22 @@ def main():
 
     # we need cut ids to display recognition results.
     args.return_cuts = True
-    librispeech = LibriSpeechAsrDataModule(args)
+    tedlium = TedAsrDataModule(args)
 
-    test_clean_cuts = librispeech.test_clean_cuts()
-    test_other_cuts = librispeech.test_other_cuts()
+    #test_clean_cuts = librispeech.test_clean_cuts()
+    #test_other_cuts = librispeech.test_other_cuts()
 
-    test_clean_dl = librispeech.test_dataloaders(test_clean_cuts)
-    test_other_dl = librispeech.test_dataloaders(test_other_cuts)
+    #test_clean_dl = librispeech.test_dataloaders(test_clean_cuts)
+    #test_other_dl = librispeech.test_dataloaders(test_other_cuts)
+
+    #test_sets = ["test-clean", "test-other"]
+    #test_dl = [test_clean_dl, test_other_dl]
+    
+    dev_cuts = tedlium.dev_cuts()
+    test_cuts = tedlium.test_cuts()
+
+    dev_dl = tedlium.valid_dataloaders(test_clean_cuts)
+    test_dl = tedlium.test_dataloaders(test_other_cuts)
 
     test_sets = ["test-clean", "test-other"]
     test_dl = [test_clean_dl, test_other_dl]
