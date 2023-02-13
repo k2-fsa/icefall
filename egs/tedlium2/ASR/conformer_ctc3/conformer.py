8581e45acf1d2bc589eab1ad0f644fe331219891
dohe0342 <kimdohe1070@gmail.com>
2023-02-14 02:49:31 +0900

    from local

--- egs/tedlium2/ASR/conformer_ctc3/conformer.py
+++ egs/tedlium2/ASR/conformer_ctc3/conformer.py
@@ -179,7 +179,7 @@ class Conformer(Transformer):
             x = 0
             for enum, alpha in enumerate(self.alpha):
                 x += self.sigmoid(alpha) * layer_outputs[(enum+1)*self.group_layer_num-1]
-            x = self.layer_norm(x/self.group_num)
+            x = self.layer_norm(x)
         
         return x, mask
 
