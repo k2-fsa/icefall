f8137825d286f64aa953c1c822a5cf09c2dc0da4
dohe0342 <kimdohe1070@gmail.com>
2023-02-14 18:29:07 +0900

    from local

--- egs/tedlium2/ASR/conformer_ctc3/conformer.py
+++ egs/tedlium2/ASR/conformer_ctc3/conformer.py
@@ -181,7 +181,12 @@ class Conformer(Transformer):
         mask = mask.to(x.device) if mask is not None else None
 
         x, layer_outputs = self.encoder(
-            x, pos_emb, src_key_padding_mask=mask, warmup=warmup, condition_layer=self.condition_layer, 
+            x, 
+            pos_emb, 
+            src_key_padding_mask=mask, 
+            warmup=warmup, 
+            condition_layer=self.condition_layer, 
+            ctc_output=self.ctc_output,
         )  # (S, N, C)
         
         if self.group_num != 0:
