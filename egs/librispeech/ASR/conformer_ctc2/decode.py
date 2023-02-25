7f65fb9fa9d620ae0bf3e76f0b51dacabac2cab4
dohe0342 <kimdohe1070@gmail.com>
2023-02-25 17:40:35 +0900

    from local

--- egs/librispeech/ASR/conformer_ctc2/decode.py
+++ egs/librispeech/ASR/conformer_ctc2/decode.py
@@ -484,6 +484,7 @@ def decode_one_batch(
     lm_scale_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
     lm_scale_list += [0.8, 0.9, 1.0, 1.1, 1.2, 1.3]
     lm_scale_list += [1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+    lm_scale_list += [2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0]
 
     if params.method == "1best":
         best_path_dict = one_best_decoding(
