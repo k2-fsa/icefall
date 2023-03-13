f269169c43c21df3f4eff7aa950019210d64e73f
dohe0342 <kimdohe1070@gmail.com>
2023-03-13 16:35:06 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/pseudo.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/pseudo.py
@@ -815,6 +815,7 @@ def main():
             if len(v) < 10:
                 continue
             utt_id = '-'.join(k.split('-')[:-1])
+            print(utt_id)
             f = open(f'/DB/LibriSpeech_tar/vox/{params.spk_id}_texts/{utt_id}.txt', 'w')
             f.write(v.strip())
             print(k, v)
