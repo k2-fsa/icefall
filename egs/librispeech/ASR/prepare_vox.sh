c77959dded8ae0a4743ef817b25d2a400a78dcd9
dohe0342 <kimdohe1070@gmail.com>
2023-03-14 16:12:43 +0900

    from local

--- egs/librispeech/ASR/prepare_vox.sh
+++ egs/librispeech/ASR/prepare_vox.sh
@@ -154,11 +154,11 @@ fi
 
 if [ $stage -le 5 ] && [ $stop_stage -ge 5 ]; then
   log "Stage 5: Generate pseudo label"
-  rm -rf $dl_dir/*_texts
+  #rm -rf $dl_dir/*_texts
   for dest in "test-clean" "test-other"; do
 	  for spk in $dl_dir/$dest/*; do
 		  spk_id=${spk#*$dest\/}
-		  ./pseudo.sh $spk_id
+		  #./pseudo.sh $spk_id
 		  #python local/prepare_vox.py $dl_dir/$dest "$spk_id"
 	  done
   done
