07c4f6ed822847d15ebd69ae44f51a15cf1d4c1d
dohe0342 <kimdohe1070@gmail.com>
2023-03-14 16:26:41 +0900

    from local

--- egs/librispeech/ASR/adapter.sh
+++ egs/librispeech/ASR/adapter.sh
@@ -4,6 +4,6 @@ for dest in "test-clean" "test-other"; do
 	for spk in $dl_dir/$dest/*; do
 		spk_id=${spk#*$dest\/}
 		echo $spk_id
-		./run_adapter.sh $spk_id
+		./run_adapter.sh "$spk_id"_adapter $spk_id
 	done
 done
