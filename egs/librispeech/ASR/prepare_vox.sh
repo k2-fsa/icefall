3f6befcc02850e8097acfb4aa9ed77347c1e7d50
dohe0342 <kimdohe1070@gmail.com>
2023-03-14 17:03:23 +0900

    from local

--- egs/librispeech/ASR/prepare_vox.sh
+++ egs/librispeech/ASR/prepare_vox.sh
@@ -165,3 +165,64 @@ if [ $stage -le 5 ] && [ $stop_stage -ge 5 ]; then
   done
 fi
 
+if [ $stage -le 6 ] && [ $stop_stage -ge 6 ]; then
+  log "Stage 1: Prepare LJSpeech manifest"
+  # We assume that you have downloaded the LJSpeech corpus (ver 1.1)
+  # You need to prepare LJSpeech according to data_settings/*_list.txt like below
+  # $dl_dir/LJSpeech
+  # |-- wavs
+  # |   |-- train
+  # |   |-- dev
+  # |   |-- test
+  # |-- texts
+  # |-- metadata.csv
+
+  # to $dl_dir/LJSpeech
+  if [ ! -e $dl_dir/vox/.vox.done ]; then
+    #for dset in "4446"; do
+    #  log "Resampling vox/$dset set"
+    #  file_list=`ls $dl_dir/vox/$dset/`
+    #  for wavfile in $file_list; do
+	#	echo $wavfile
+    #    sox -v 0.9 $dl_dir/vox/$dset/$wavfile -r 16000 -e signed-integer $dl_dir/vox/$dset/tmp_$wavfile
+    #    mv $dl_dir/vox/$dset/tmp_$wavfile $dl_dir/vox/$dset/$wavfile
+    #  done
+    #  log "Resampling $dset done"
+    #done
+
+  mkdir -p data/manifests
+  if [ ! -e data/manifests/.vox.done ]; then
+	for dest in "test-clean" "test-other"; do
+		for spk in $dl_dir/$dest/*; do
+			spk_id=${spk#*$dest\/}
+			python local/prepare_vox.py $dl_dir/$dest "$spk_id"
+		done
+	done
+    #touch data/manifests/.vox.done
+  fi
+fi
+
+if [ $stage -le 7 ] && [ $stop_stage -ge 7 ]; then
+  log "Stage 7: Prepare musan manifest"
+  # We assume that you have downloaded the musan corpus
+  # to data/musan
+  mkdir -p data/manifests
+  if [ ! -e data/manifests/.musan.done ]; then
+    lhotse prepare musan $dl_dir/musan data/manifests
+    touch data/manifests/.musan.done
+  fi
+fi
+
+if [ $stage -le 3 ] && [ $stop_stage -ge 3 ]; then
+  log "Stage 3: Compute fbank for Vox"
+  mkdir -p data/fbank
+  if [ ! -e data/fbank/.LJSpeech.done ]; then
+	  for dest in "test-clean" "test-other"; do
+		  for spk in $dl_dir/$dest/*; do
+			  spk_id=${spk#*$dest\/}
+			  ./local/compute_fbank_vox.py --data-dir $spk --spk-id $spk_id
+		  done
+	  done
+    #touch data/fbank/.vox.done
+  fi
+
