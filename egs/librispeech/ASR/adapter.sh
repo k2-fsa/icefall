50e419fbf562b7957f780922726c095d1a940033
dohe0342 <kimdohe1070@gmail.com>
2023-03-14 22:58:55 +0900

    from local

--- egs/librispeech/ASR/adapter.sh
+++ egs/librispeech/ASR/adapter.sh
@@ -4,7 +4,7 @@ for dest in "test-clean" "test-other"; do
 	for spk in $dl_dir/$dest/*; do
 		spk_id=${spk#*$dest\/}
 		echo $spk_id
-		if [$spk_id -ne 1089]; then
+		if [ $spk_id -ne 1089 ]; then
 			./run_adapter.sh "$spk_id"_adapter $spk_id
 		fi
 	done
