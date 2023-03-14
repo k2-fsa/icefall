43fa35b9cb6f2e4c0e6083160594e0e860a6b575
dohe0342 <kimdohe1070@gmail.com>
2023-03-15 02:59:14 +0900

    from local

--- egs/librispeech/ASR/adapter.sh
+++ egs/librispeech/ASR/adapter.sh
@@ -1,11 +1,9 @@
 dl_dir=/DB/LibriSpeech_tar/vox
 
-for dest in "test-clean" "test-other"; do
+for dest in "test-other"; do
 	for spk in $dl_dir/$dest/*; do
 		spk_id=${spk#*$dest\/}
 		echo $spk_id
-		if [ $spk_id -ne 1089 ]; then
-			./run_adapter.sh "$spk_id"_adapter $spk_id
-		fi
+		./run_adapter.sh "$spk_id"_adapter $spk_id
 	done
 done
