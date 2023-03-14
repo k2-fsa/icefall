1452abdab24167b48f636ae878491ab76eca6658
dohe0342 <kimdohe1070@gmail.com>
2023-03-14 16:25:30 +0900

    from local

--- egs/librispeech/ASR/adapter.sh
+++ egs/librispeech/ASR/adapter.sh
@@ -1,6 +1,7 @@
 dl_dir=/DB/LibriSpeech_tar/vox
+
 for dest in "test-clean" "test-other"; do
-      for spk in $dl_dir/$dest/*; do
-          spk_id=${spk#*$dest\/}
+	for spk in $dl_dir/$dest/*; do
+		spk_id=${spk#*$dest\/}
           echo $spk_id
           ./pseudo.sh $spk_id
