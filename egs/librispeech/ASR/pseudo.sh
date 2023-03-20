c2de71c6d5db44c2be333a3aa80edcbfeda6899c
dohe0342 <kimdohe1070@gmail.com>
2023-03-20 11:29:26 +0900

    from local

--- egs/librispeech/ASR/pseudo.sh
+++ egs/librispeech/ASR/pseudo.sh
@@ -2,7 +2,7 @@ git pull
 
 spk_id=$1
 
-for method in greedy_search; do #modified_beam_search fast_beam_search; do
+for method in modified_beam_search; do #modified_beam_search fast_beam_search; do
 	  ./pruned_transducer_stateless_d2v_v2/pseudo.py \
 		--input-strategy AudioSamples \
 		--enable-spec-aug False \
