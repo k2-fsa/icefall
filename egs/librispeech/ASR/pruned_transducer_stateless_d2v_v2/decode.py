8616bdefb59a86d554abc306af2a62a201f7a6b9
dohe0342 <kimdohe1070@gmail.com>
2023-03-21 13:43:05 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -594,7 +594,7 @@ def save_results(
     for key, val in test_set_wers:
         s += "{}\t{}{}\n".format(key, val, note)
         note = ""
-        spk = note.split(' ')[-1]
+        spk = note.split(' ')[-2]
         wer = str(val)
     logging.info(s)
     with open(f'./{params.res_name}.txt', 'a') as f:
