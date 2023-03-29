a8bba953c491f1edb46684348c8bf651b080c506
dohe0342 <kimdohe1070@gmail.com>
2023-03-29 22:09:45 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
@@ -351,7 +351,7 @@ def get_parser():
     parser.add_argument(
         "--num-updates",
         type=int,
-        default=30,
+        default=5000,
         help="Number of epochs to train.",
     )
 
