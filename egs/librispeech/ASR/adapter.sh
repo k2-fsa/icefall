dc699be027440a488ac302db4a53a8f5890592fe
dohe0342 <kimdohe1070@gmail.com>
2023-04-03 18:50:58 +0900

    from local

--- egs/librispeech/ASR/adapter.sh
+++ egs/librispeech/ASR/adapter.sh
@@ -1,5 +1,6 @@
 #Dl_dir=/DB/LibriSpeech_tar/vox
-dl_dir=/home/work/workspace/LibriSpeech/vox_v3
+Dl_dir=/DB/LibriSpeech_tar/vox_v3
+#dl_dir=/home/work/workspace/LibriSpeech/vox_v3
 
 #for dest in "test-clean" "test-other"; do
 for dest in "test-clean" "test-other"; do
