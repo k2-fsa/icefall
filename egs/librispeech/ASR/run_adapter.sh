74b288499bf25df482897f939d2266e8e0a2ed14
dohe0342 <kimdohe1070@gmail.com>
2023-04-03 18:27:24 +0900

    from local

--- egs/librispeech/ASR/run_adapter.sh
+++ egs/librispeech/ASR/run_adapter.sh
@@ -52,6 +52,38 @@ if [ $workstation = "whale" ]; then
 #		--joiner-dim 768 \
 #		--prune-range 10 
 else
+	export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+	if [ ! -e ./pruned_transducer_stateless_d2v_v2/$1/.train.done ]; then
+		./pruned_transducer_stateless_d2v_v2/train_adapter.py \
+			--num-buckets 2 \
+			--add-adapter True \
+			--adapter-lr 0.02 \
+			--gender male \
+			--wandb False \
+			--input-strategy AudioSamples \
+			--enable-spec-aug False \
+			--multi-optim False \
+			--world-size 8 \
+			--num-epochs 10000 \
+			--num-updates 501 \
+			--save-every-n 100 \
+			--full-libri 1 \
+			--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
+			--max-duration 200 \
+			--encoder-dim 768 \
+			--decoder-dim 768 \
+			--joiner-dim 768 \
+			--use-fp16 0 \
+			--accum-grads 1 \
+			--encoder-type d2v \
+			--additional-block True \
+			--prune-range 10 \
+			--spk-id $2 
+		touch ./pruned_transducer_stateless_d2v_v2/$1/.train.done
+	fi
+
+'''
+else
 	export CUDA_VISIBLE_DEVICES="0,1,2,3"
 	./pruned_transducer_stateless_d2v_v2/train_adapter.py \
 		--num-buckets 2 \
@@ -79,3 +111,4 @@ else
 		--simple-loss-scale 0.2869 \
 		--spk-id $2 
 fi
+'''
