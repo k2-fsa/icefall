d4bf52ec61edbd3199c710abff987feb911cc739
dohe0342 <kimdohe1070@gmail.com>
2023-04-04 00:47:13 +0900

    from local

--- egs/librispeech/ASR/run_adapter.sh
+++ egs/librispeech/ASR/run_adapter.sh
@@ -83,33 +83,31 @@ else
 		touch ./pruned_transducer_stateless_d2v_v2/$1/.train.done
 	fi
 
-'''
-else
-	export CUDA_VISIBLE_DEVICES="0,1,2,3"
-	./pruned_transducer_stateless_d2v_v2/train_adapter.py \
-		--num-buckets 2 \
-		--add-adapter True \
-		--adapter-lr 0.02472 \
-		--wandb False \
-		--input-strategy AudioSamples \
-		--enable-spec-aug False \
-		--multi-optim False \
-		--world-size 4 \
-		--num-epochs 31 \
-		--full-libri 1 \
-		--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
-		--max-duration 150 \
-		--encoder-dim 768 \
-		--decoder-dim 768 \
-		--joiner-dim 768 \
-		--use-fp16 0 \
-		--accum-grads 4 \
-		--encoder-type d2v \
-		--additional-block True \
-		--prune-range 10 \
-		--ctc-loss-scale 0.1924 \
-		--lm-scale 0.1254 \
-		--simple-loss-scale 0.2869 \
-		--spk-id $2 
-fi
-'''
+#else
+#	export CUDA_VISIBLE_DEVICES="0,1,2,3"
+#	./pruned_transducer_stateless_d2v_v2/train_adapter.py \
+#		--num-buckets 2 \
+#		--add-adapter True \
+#		--adapter-lr 0.02472 \
+#		--wandb False \
+#		--input-strategy AudioSamples \
+#		--enable-spec-aug False \
+#		--multi-optim False \
+#		--world-size 4 \
+#		--num-epochs 31 \
+#		--full-libri 1 \
+#		--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
+#		--max-duration 150 \
+#		--encoder-dim 768 \
+#		--decoder-dim 768 \
+#		--joiner-dim 768 \
+#		--use-fp16 0 \
+#		--accum-grads 4 \
+#		--encoder-type d2v \
+#		--additional-block True \
+#		--prune-range 10 \
+#		--ctc-loss-scale 0.1924 \
+#		--lm-scale 0.1254 \
+#		--simple-loss-scale 0.2869 \
+#		--spk-id $2 
+#fi
