a51a122846864b0e5b103c601f1de0b7f9f3ff23
dohe0342 <kimdohe1070@gmail.com>
2023-04-11 16:17:51 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/prompt_tuning.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/prompt_tuning.py
@@ -1604,7 +1604,7 @@ def run_adapter(rank, world_size, args, wb=None):
         )
     '''
 
-    prompt = torch.randn((50, 512), requires_grad=True)
+    prompt = torch.randn((100, 512), requires_grad=True)
     optimizer_adapter = ScaledAdam(
             [prompt],
             lr=params.adapter_lr,
