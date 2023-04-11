4bb95809b0df32b18fdc582c1b5d615716844123
dohe0342 <kimdohe1070@gmail.com>
2023-04-11 16:45:39 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/prompt_tuning.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/prompt_tuning.py
@@ -588,7 +588,7 @@ def get_params() -> AttributeDict:
             "best_train_epoch": -1,
             "best_valid_epoch": -1,
             "batch_idx_train": 0,
-            "log_interval": 20,
+            "log_interval": 5,
             "reset_interval": 200,
             "valid_interval": 3000,  # For the 100h subset, use 800
             # parameters for zipformer
