1483a89bea095200df45f03e9a2314cfcfb703df
dohe0342 <kimdohe1070@gmail.com>
2023-04-12 01:53:21 +0900

    from local

--- egs/librispeech/ASR/prompt_tuning_all.sh
+++ egs/librispeech/ASR/prompt_tuning_all.sh
@@ -1,7 +1,7 @@
 #dl_dir=/DB/LibriSpeech_tar/vox
 subset=$1
-dl_dir=/DB/LibriSpeech_tar/$subset
-#dl_dir=/home/work/workspace/LibriSpeech/vox_v3
+#dl_dir=/DB/LibriSpeech_tar/$subset
+dl_dir=/home/work/workspace/LibriSpeech/vox_v3
 
 for dest in "test-clean" "test-other"; do
 #for dest in "test-other"; do
