af69a706babb177696690750142d79810bb22f9e
dohe0342 <kimdohe1070@gmail.com>
2023-04-11 14:00:59 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_adapter.py
@@ -740,6 +740,7 @@ def load_checkpoint_if_available(
             params["cur_batch_idx"] = saved_params["cur_batch_idx"]
 
     params.batch_idx_train = 0
+    print(saved_params)
 
     return saved_params
 
