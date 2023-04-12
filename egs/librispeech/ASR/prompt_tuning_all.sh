82ea61e1afa1027be51d82842765b3937fec650a
dohe0342 <kimdohe1070@gmail.com>
2023-04-12 14:25:30 +0900

    from local

--- egs/librispeech/ASR/prompt_tuning_all.sh
+++ egs/librispeech/ASR/prompt_tuning_all.sh
@@ -8,6 +8,6 @@ for dest in "test-clean" "test-other"; do
 	for spk in $dl_dir/$dest/*; do
 		spk_id=${spk#*$dest\/}
 		echo $spk_id
-		./prompt_tuning.sh prompt_tuning_"$spk_id" $spk_id $1
+		./prompt_tuning.sh prompt_tuning_"$spk_id" $spk_id $subset
 	done
 done
