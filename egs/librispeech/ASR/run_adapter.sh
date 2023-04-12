5e3990fbee854b9233d99b468aebdf0239b8a0ae
dohe0342 <kimdohe1070@gmail.com>
2023-04-12 13:29:44 +0900

    from local

--- egs/librispeech/ASR/run_adapter.sh
+++ egs/librispeech/ASR/run_adapter.sh
@@ -67,8 +67,8 @@ else
 			--multi-optim False \
 			--world-size 4 \
 			--num-epochs 10000 \
-			--num-updates 201 \
-			--save-every-n 100 \
+			--num-updates 101 \
+			--save-every-n 50 \
 			--full-libri 1 \
 			--exp-dir ./pruned_transducer_stateless_d2v_v2/$1 \
 			--max-duration 150 \
