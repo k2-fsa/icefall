5cf78cda453417c5b2bb1be946421f62671afb88
dohe0342 <kimdohe1070@gmail.com>
2023-04-13 22:04:21 +0900

    from local

--- egs/librispeech/ASR/decode.sh
+++ egs/librispeech/ASR/decode.sh
@@ -1,9 +1,10 @@
-dl_dir=/DB/LibriSpeech_tar/vox_10m
+dl_dir=/DB/LibriSpeech_tar/vox_30m
 #dl_dir=/home/work/workspace/LibriSpeech/vox_v3
 for dest in "test-clean" "test-other"; do
 	for spk in $dl_dir/$dest/*; do
 		spk_id=${spk#*$dest\/}
 		echo $spk_id
-		./test.sh $spk_id prompt_tuning_"$spk_id"
+		#./test.sh $spk_id prompt_tuning_"$spk_id"
+		./test.sh $spk_id "$spk_id"_adapter_30m
 	done
 done
