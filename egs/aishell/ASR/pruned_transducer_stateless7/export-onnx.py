75d9647117ccb06aed85067ddcd28e775b00241f
marcoyang1998 <45973641+marcoyang1998@users.noreply.github.com>
2023-04-17 17:26:35 +0800

    Update egs/aishell/ASR/pruned_transducer_stateless7/export-onnx.py
    Co-authored-by: Fangjun Kuang <csukuangfj@gmail.com>

--- egs/aishell/ASR/pruned_transducer_stateless7/export-onnx.py
+++ egs/aishell/ASR/pruned_transducer_stateless7/export-onnx.py
@@ -134,7 +134,7 @@ def get_parser():
     parser.add_argument(
         "--context-size",
         type=int,
-        default=2,
+        default=1,
         help="The context size in the decoder. 1 means bigram; 2 means tri-gram",
     )
 
