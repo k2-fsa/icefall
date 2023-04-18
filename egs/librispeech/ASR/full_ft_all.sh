f0bbb38ecba534c3892001cff7ceea1573a0c0d5
dohe0342 <kimdohe1070@gmail.com>
2023-04-18 14:25:02 +0900

    from local

--- egs/librispeech/ASR/full_ft_all.sh
+++ egs/librispeech/ASR/full_ft_all.sh
@@ -1,5 +1,4 @@
 #dl_dir=/DB/LibriSpeech_tar/vox
-subset=$1
 dl_dir=/DB/LibriSpeech_tar/$subset
 #dl_dir=/home/work/workspace/LibriSpeech/vox_v3
 
@@ -8,6 +7,6 @@ for dest in "test-clean" "test-other"; do
 	for spk in $dl_dir/$dest/*; do
 		spk_id=${spk#*$dest\/}
 		echo $spk_id
-		./full_ft.sh "$spk_id"_fullft $spk_id $subset
+		./full_ft.sh "$spk_id"_fullft $spk_id
 	done
 done
