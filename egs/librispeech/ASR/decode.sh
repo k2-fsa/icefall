c4f9352b642e5fe346fd73752e47579b2367a16d
dohe0342 <kimdohe1070@gmail.com>
2023-04-21 10:50:31 +0900

    from local

--- egs/librispeech/ASR/decode.sh
+++ egs/librispeech/ASR/decode.sh
@@ -4,7 +4,7 @@ for dest in "test-clean" "test-other"; do
 	for spk in $dl_dir/$dest/*; do
 		spk_id=${spk#*$dest\/}
 		echo $spk_id
-		./test.sh $spk_id prompt_tuning_20_$spk_id
+		./test.sh $spk_id prompt_tuning_10_$spk_id
 		#./test.sh $spk_id "$spk_id"_fullft
 		#./test.sh $spk_id prompt_tuning_"$spk_id"
 		#./test.sh $spk_id "$spk_id"_adapter_30m
