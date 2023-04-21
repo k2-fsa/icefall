275fe08e5786641c396cc977d44762c7716216db
dohe0342 <kimdohe1070@gmail.com>
2023-04-21 11:44:58 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -361,8 +361,7 @@ def decode_one_batch(
     
     if 1:
         encoder_out = encoder_out[:,50:,:]
-        print(encoder_out_lens)
-        exit()
+        encoder_out -= 50
     
     hyps = []
 
