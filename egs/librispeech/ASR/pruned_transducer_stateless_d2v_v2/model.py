28a2523331a060b008e0562400d852b8a74c1132
dohe0342 <kimdohe1070@gmail.com>
2023-04-25 12:08:08 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/model.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/model.py
@@ -80,7 +80,7 @@ class Transducer(nn.Module):
         self.prompt = None
         if prompt:
             #self.prompt = torch.randn((200, 512), requires_grad=True)
-            self.prompt = torch.nn.Parameter(torch.randn((100, 512)))
+            self.prompt = torch.nn.Parameter(torch.rand((100, 512)))
 
     def forward(
         self,
