13b568f52f8f9b17d5d46e08cbd710336e702c7a
dohe0342 <kimdohe1070@gmail.com>
2023-04-26 14:57:54 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/model.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/model.py
@@ -87,6 +87,8 @@ class Transducer(nn.Module):
             new_emb = torch.empty(512, 50)
             for i in range(512):
                 mean, std = statistic[i].strip().split(' ')
+                print(new_emb[i].size())
+                print(float(mean), float(std))
                 new_emb[i] = torch.normal(float(mean), float(std), size=(50)).squeeze()
             new_emb = new_emb.transpose(1,0)
             self.prompt = torch.nn.Parameter(new_emb) 
