5914926b9fb028cbc2cbf0a962382b5628fea3f0
dohe0342 <kimdohe1070@gmail.com>
2023-04-27 18:45:09 +0900

    from local

--- egs/librispeech/ASR/full_ft.sh
+++ egs/librispeech/ASR/full_ft.sh
@@ -41,7 +41,7 @@ else
 		--max-duration 150 \
 		--freeze-finetune-updates 0 \
 		--num-epochs 10000 \
-		--num-updates 101 \
+		--num-updates 127873 \
 		--save-every-n 50 \
 		--encoder-dim 768 \
 		--decoder-dim 768 \
