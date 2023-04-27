9609b959dcd5bf0d2ce1a5280ad687d944f4dbe4
dohe0342 <kimdohe1070@gmail.com>
2023-04-27 20:33:57 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/model.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/model.py
@@ -84,10 +84,9 @@ class Transducer(nn.Module):
         print(self.sid)
         print('-'*20)
         if prompt:
-            #self.prompt = torch.randn((200, 512), requires_grad=True)
-            #statistic = open(f'/home/work/workspace/icefall/egs/librispeech/ASR/conv_feat/{self.sid}/{sid}_statistic.txt', 'r').readlines()
+            self.prompt = torch.randn((200, 512), requires_grad=True)
+            statistic = open(f'/home/work/workspace/icefall/egs/librispeech/ASR/conv_feat/{self.sid}/{sid}_statistic.txt', 'r').readlines()
             self.prompt = torch.nn.Parameter(torch.rand((50, 512)))
-            '''
             print(self.prompt)
             
             new_emb = torch.empty(512, 50)
@@ -99,7 +98,6 @@ class Transducer(nn.Module):
             new_emb = new_emb.transpose(1,0)
             self.prompt = torch.nn.Parameter(new_emb) 
             print(self.prompt)
-            '''
 
     def forward(
         self,
