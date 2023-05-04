951e41dede7ab8f1438e01e65623ab6035a66297
dohe0342 <kimdohe1070@gmail.com>
2023-05-04 13:46:18 +0900

    from local

--- egs/librispeech/ASR/lastlayer_all.sh
+++ egs/librispeech/ASR/lastlayer_all.sh
@@ -8,7 +8,7 @@ for dest in "test-clean" "test-other"; do
 	for spk in $dl_dir/$dest/*; do
 		spk_id=${spk#*$dest\/}
 		echo $spk_id
-		./lastlayer.sh bitfit_"$spk_id"_v2 $spk_id bear
+		./lastlayer.sh lastlayer $spk_id bear
 	done
 done
 
