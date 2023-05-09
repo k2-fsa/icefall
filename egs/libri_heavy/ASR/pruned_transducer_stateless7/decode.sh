d4f6e056eaf54c4c5c3d8b4d4dab856e9f023ff8
marcoyang <marcoyang1998@gmail.com>
2023-05-09 11:54:29 +0800

    minor updates

--- egs/libri_heavy/ASR/pruned_transducer_stateless7/decode.sh
+++ egs/libri_heavy/ASR/pruned_transducer_stateless7/decode.sh
@@ -10,11 +10,11 @@ echo "Using device: ${CUDA_VISIBLE_DEVICES}"
 
 for avg in 10; do
     ./pruned_transducer_stateless7/decode.py \
-        --epoch 40 \
+        --epoch 60 \
         --avg $avg \
         --use-averaged-model True \
-        --exp-dir ./pruned_transducer_stateless7/exp-medium-replace-full-width-4gpus \
+        --exp-dir ./pruned_transducer_stateless7/exp_small_uppercase_ref_4gpus \
+        --bpe-model /ceph-data4/yangxiaoyu/softwares/icefall_development/icefall_libri_light/egs/libri_heavy/ASR/data/lang_bpe_500/bpe.model \
         --max-duration 1200 \
-        --random-left-padding False \
         --decoding-method greedy_search
 done
--- egs/libri_heavy/ASR/pruned_transducer_stateless7/run.sh
+++ egs/libri_heavy/ASR/pruned_transducer_stateless7/run.sh
@@ -4,17 +4,18 @@
 
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 export PYTHONPATH=~/softwares/icefall_development/icefall_libri_light:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
 echo "Using device: ${CUDA_VISIBLE_DEVICES}"
 
+subset=small
 python ./pruned_transducer_stateless7/train.py \
-    --world-size 1 \
-    --start-epoch 1 \
-    --exp-dir ./pruned_transducer_stateless7/exp-debug \
+    --world-size 4 \
+    --start-epoch 7 \
+    --exp-dir ./pruned_transducer_stateless7/exp-${subset}-fixed-start-full-width-md450-4gpus \
     --use-fp16 True \
-    --num-epochs 40 \
-    --subset medium \
-    --manifest-dir data/fbank \
-    --max-duration 500 \
-    --master-port 13782
+    --num-epochs 60 \
+    --subset $subset \
+    --manifest-dir data/fbank_new \
+    --max-duration 450 \
+    --master-port 13780
