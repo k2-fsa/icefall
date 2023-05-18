fa658ce0cf8a0a8b6459c6e14f4151cd099df70e
dohe0342 <kimdohe1070@gmail.com>
2023-05-18 15:59:45 +0900

    from local

---
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/bias_compare.py
@@ -0,0 +1,10 @@
+import torch
+
+base_model = torch.load('./d2v-base-T.pt')
+bias_model = torch.load('./bitfit_2414_v2/checkpoint-100.pt')
+
+base_model, bias_model = base_model['model'], bias_model['model']
+
+for key in base_model.keys():
+    if 'bias' in key:
+        print(key, base_model[key], bias_model[key])
