383cb553f4d3e8c0ebdf90b6dd48716d3f0097ee
dohe0342 <kimdohe1070@gmail.com>
2023-05-18 17:09:55 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
@@ -270,35 +270,6 @@ class LoRAModule(nn.Module):
         x = x.transpose(0, 1)
         return x
 
-    
-    '''
-        self.type = 'linear'
-        
-        def build_adapter(embedding_dim, proj_dim, type_=self.type):
-            if type_ == 'conv':
-                return ConvolutionModule(768, 31)
-            else:
-                return nn.Sequential(
-                    #nn.LayerNorm(embedding_dim),
-                    nn.Linear(embedding_dim, proj_dim),
-                    nn.ReLU(),
-                    nn.Linear(proj_dim, embedding_dim),
-                    nn.LayerNorm(embedding_dim),
-                )
-
-        self.adapter_layers = nn.ModuleList(
-            [build_adapter(embedding_dim, proj_dim, type_=self.type) for _ in range(layer_num)]
-        )
-    
-    def forward(self, x, layer_id=-1):
-        x = x.transpose(0, 1)
-        residual = x
-        x = self.adapter_layers[layer_id](x)
-        x = residual + x
-        x = x.transpose(0, 1)
-
-        return x
-    '''
 
 class ResidualAdapterModule(nn.Module):
     """
