a57e992af22dfda733ad8c02b880782102a40445
Fangjun Kuang <csukuangfj@gmail.com>
2023-05-25 07:37:01 +0800

    remove outdated code in train.py

--- egs/librispeech/ASR/transducer/train.py
+++ egs/librispeech/ASR/transducer/train.py
@@ -627,14 +627,6 @@ def run(rank, world_size, args):
 
     train_cuts = train_cuts.filter(remove_short_and_long_utt)
 
-    num_left = len(train_cuts)
-    num_removed = num_in_total - num_left
-    removed_percent = num_removed / num_in_total * 100
-
-    logging.info(f"Before removing short and long utterances: {num_in_total}")
-    logging.info(f"After removing short and long utterances: {num_left}")
-    logging.info(f"Removed {num_removed} utterances ({removed_percent:.5f}%)")
-
     train_dl = librispeech.train_dataloaders(train_cuts)
 
     valid_cuts = librispeech.dev_clean_cuts()
