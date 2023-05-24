28bc046e2a48ad05c09055c9ebdb7c82c32290b0
dohe0342 <kimdohe1070@gmail.com>
2023-05-24 13:34:07 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
@@ -163,7 +163,7 @@ class LoRAModule(nn.Module):
 
     def reset_parameters(self):
         nn.init.zeros_(self.lora_B.weight.data)
-        nn.init_normal_(self.lora_A.weight.dat)
+        nn.init.normal_(self.lora_A.weight.data)
 
     def forward(self, x):
         x = x.transpose(0, 1)
