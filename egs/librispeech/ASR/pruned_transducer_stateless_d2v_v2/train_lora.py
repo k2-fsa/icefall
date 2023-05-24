2ddec73e3a01668b3183ed9812a2a26b3daf475b
dohe0342 <kimdohe1070@gmail.com>
2023-05-24 12:43:13 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_lora.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_lora.py
@@ -131,7 +131,11 @@ LRSchedulerType = Union[torch.optim.lr_scheduler._LRScheduler, optim.LRScheduler
 class LoRAHook():
     def __init__(self, module):
         self.hook = module.register_forward_hook(self.hook_fn)
-
+        self.lora = LoRAModule(
+                       embedding_dim=768,
+                       rank=4,
+                       lora_alpha=1.0,
+                    )
     def hook_fn(self, module, input, output):
 
 
