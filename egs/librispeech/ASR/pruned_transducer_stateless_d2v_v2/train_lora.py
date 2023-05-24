fd5c0dca7710c5633b50fda4591b1738d5499cdb
dohe0342 <kimdohe1070@gmail.com>
2023-05-24 13:46:53 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_lora.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_lora.py
@@ -139,7 +139,7 @@ class LoRAHook():
                            lora_alpha=1.0,
                     )
     def hook_fn(self, module, input, output):
-        print(input)
+        print(module, input, output))
         lora_out = self.lora(input)
         output = input + lora_out
 
