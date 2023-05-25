e79775a4cc6d01e62ecbfc44ba136a81e822983e
dohe0342 <dohe1070@gmail.com>
2023-05-25 21:57:04 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -699,6 +699,7 @@ def main():
         for i, lora in enumerate(lora_modules):
             lora_param = torch.load(f"{params.exp_dir}/lora_{params.iter}_{i}.pt")
             lora.lora.load_state_dict(lora_param)
+            lora.lora.to(device)
         logging.info("lora params load done")
 
     else:
