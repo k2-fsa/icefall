ef8c5476823a347f80ed80e26fbd3f337dbaa924
dohe0342 <dohe1070@gmail.com>
2023-05-25 19:26:24 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_lora.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/train_lora.py
@@ -1610,7 +1610,7 @@ def run_adapter(rank, world_size, args, wb=None):
             #    if rank == 0: logging.info(module)
                 #print(module)
                 if isinstance(module, torch.nn.Linear):
-                    if rank == 0: logging.info(module)
+                    if rank == 0: print(module)
                 #    lora_modules.append(LoRAHook(module))
     
     adapter_names = []
