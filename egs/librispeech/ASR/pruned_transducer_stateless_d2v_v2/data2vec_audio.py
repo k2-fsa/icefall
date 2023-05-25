5c3819098d8674d79aab1ee0c31130fbf20285d0
dohe0342 <dohe1070@gmail.com>
2023-05-25 21:05:30 +0900

    from local

--- egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
+++ egs/librispeech/ASR/pruned_transducer_stateless_d2v_v2/data2vec_audio.py
@@ -171,10 +171,10 @@ class LoRAModule(nn.Module):
         nn.init.normal_(self.lora_A.bias.data)
 
     def forward(self, x):
-        x = x.transpose(0, 1)
+        #x = x.transpose(0, 1)
         x = self.lora_A(x)
         x = self.lora_B(x)
-        x = x.transpose(0, 1)
+        #x = x.transpose(0, 1)
         x *= self.scaling
         return x
 
