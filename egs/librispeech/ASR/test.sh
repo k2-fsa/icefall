1173d25853ca44588f5a8273e41fe79f367b6e62
dohe0342 <kimdohe1070@gmail.com>
2023-05-26 17:16:16 +0900

    from local

--- egs/librispeech/ASR/test.sh
+++ egs/librispeech/ASR/test.sh
@@ -14,7 +14,7 @@ do
 		--enable-spec-aug False \
 		--additional-block True \
 		--exp-dir ./pruned_transducer_stateless_d2v_v2/$2 \
-		--max-duration 30 \
+		--max-duration 600 \
 		--iter $i \
 		--model-name lora \
 		--decoding-method $method \
