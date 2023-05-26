787cb9f2c0efb6f72fb0acf06071c56556f2e114
dohe0342 <kimdohe1070@gmail.com>
2023-05-26 16:42:39 +0900

    from local

--- egs/librispeech/ASR/decode.sh
+++ egs/librispeech/ASR/decode.sh
@@ -8,7 +8,8 @@ for dest in "test-clean" "test-other"; do
 		#./test.sh $spk_id prompt_tuning_100_$spk_id
 		#./test.sh $spk_id encoderfreeze_$spk_id
 		#./test.sh $spk_id encoderlast2_$spk_id
-		./test.sh $spk_id bitfit_"$spk_id"_q_fc1
+		#./test.sh $spk_id bitfit_"$spk_id"_q_fc1
+		./test.sh $spk_id lora_$spk_id
 		#./test.sh $spk_id self_init_"$spk_id"
 		#./test.sh $spk_id prompt_tuning_"$spk_id"
 		#./test.sh $spk_id "$spk_id"_adapter_30m
