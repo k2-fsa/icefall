ebb4ff7bd2058340620553f353d698e0cb465ee6
dohe0342 <kimdohe1070@gmail.com>
2023-06-09 15:58:44 +0900

    from local

--- egs/tedlium3/ASR/lora.sh
+++ egs/tedlium3/ASR/lora.sh
@@ -37,11 +37,10 @@ else
 	export CUDA_VISIBLE_DEVICES="0,1,2,3"
 	#rm ./pruned_transducer_stateless_d2v_v2/$1/.train.done
 	if [ ! -e ./pruned_transducer_stateless_d2v_v2/$1/.train.done ]; then
-		./pruned_transducer_stateless_d2v_v2/train_lora.py \
+		./pruned_transducer_stateless_d2v_v2/train_tta.py \
 			--num-buckets 2 \
 			--add-adapter True \
 			--adapter-lr 0.01 \
-			--gender male \
 			--wandb False \
 			--input-strategy AudioSamples \
 			--enable-spec-aug False \
