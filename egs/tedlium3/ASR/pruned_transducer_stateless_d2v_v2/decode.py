cbe026f8ff263e994330aa66249728b253fff833
dohe0342 <kimdohe1070@gmail.com>
2023-06-09 17:26:50 +0900

    from local

--- egs/tedlium3/ASR/pruned_transducer_stateless_d2v_v2/decode.py
+++ egs/tedlium3/ASR/pruned_transducer_stateless_d2v_v2/decode.py
@@ -823,47 +823,10 @@ def main():
 
     # we need cut ids to display recognition results.
     args.return_cuts = True
-    librispeech = LibriSpeechAsrDataModule(args)
+    tedlium = TedLiumAsrDataModule(args)
+    test_cuts = tedlium.test_cuts()
+    test_dl = tedlium.train_dataloaders(train_cuts)
     
-    #test_clean_cuts = librispeech.test_clean_cuts(option='male')
-    #test_other_cuts = librispeech.test_other_cuts(option='male')
-    if 0:
-        test_clean_cuts = librispeech.test_clean_user(option=option)
-        test_other_cuts = librispeech.test_other_user(option=option)
-        test_clean_dl = librispeech.test_dataloaders(test_clean_cuts)
-        test_other_dl = librispeech.test_dataloaders(test_other_cuts)
-        test_sets = [f"test-clean", f"test-other"]
-        test_dl = [test_clean_dl, test_other_dl]
-
-    if 0:
-        option = 'big'
-        test_clean_cuts = librispeech.test_clean_user(option=option)
-        test_other_cuts = librispeech.test_other_user(option=option)
-        test_clean_dl = librispeech.test_dataloaders(test_clean_cuts)
-        test_other_dl = librispeech.test_dataloaders(test_other_cuts)
-
-        test_sets = [f"test-clean_sampling"]
-        test_dl = [test_clean_dl]
-
-        #test_sets = [f"test-other_sampling"]
-        #test_dl = [test_other_dl]
-
-        #test_sets = [f"test-clean_sampling", f"test-other_sampling"]
-        #test_dl = [test_clean_dl, test_other_dl]
-    
-    if 0:
-        option = '6938'
-        test_clean_cuts = librispeech.vox_cuts(option=option)
-        test_clean_dl = librispeech.test_dataloaders(test_clean_cuts)
-        test_sets = [f"test-clean_sampling"]
-        test_dl = [test_clean_dl]
-    
-    if 1:
-        test_clean_cuts = librispeech.userlibri_cuts(option=params.spk_id)
-        test_clean_dl = librispeech.test_dataloaders(test_clean_cuts)
-        test_sets = [f"{params.spk_id}"]
-        test_dl = [test_clean_dl]
-
     for test_set, test_dl in zip(test_sets, test_dl):
         results_dict = decode_dataset(
             dl=test_dl,
