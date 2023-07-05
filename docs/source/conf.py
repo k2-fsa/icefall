8b6e8d0bd441533d08efd4177e774274a91291a8
marcoyang <marcoyang1998@gmail.com>
2023-07-05 14:46:18 +0800

    add item

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,6 +86,7 @@ rst_epilog = """
 .. _git-lfs: https://git-lfs.com/
 .. _ncnn: https://github.com/tencent/ncnn
 .. _LibriSpeech: https://www.openslr.org/12
+.. _Gigaspeech: https://github.com/SpeechColab/GigaSpeech
 .. _musan: http://www.openslr.org/17/
 .. _ONNX: https://github.com/onnx/onnx
 .. _onnxruntime: https://github.com/microsoft/onnxruntime
