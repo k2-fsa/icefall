bd053ca90aa67ddc482709722de0971014d3f635
JinZr <60612200+JinZr@users.noreply.github.com>
2023-09-15 11:00:20 +0800

    minor fix

--- icefall/checkpoint.py
+++ icefall/checkpoint.py
@@ -76,9 +76,7 @@ def save_checkpoint(
 
     if isinstance(model, DDP):
         model = model.module
-    # import pdb
-
-    # pdb.set_trace()
+    
     checkpoint = {
         "model": model.state_dict(),
         "optimizer": optimizer.state_dict() if optimizer is not None else None,
