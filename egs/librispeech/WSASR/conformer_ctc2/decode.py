b8aad7b9f2eabd382eae15bbe44baedacf8a46a4
Dongji Gao <dgao5@jhu.edu>
2023-09-19 10:46:55 -0400

    fixed the default value of otc-token in decode.py

--- egs/librispeech/WSASR/conformer_ctc2/decode.py
+++ egs/librispeech/WSASR/conformer_ctc2/decode.py
@@ -68,7 +68,7 @@ def get_parser():
     )
 
     parser.add_argument(
-        "--otc-token", type=str, default="▁<star>", help="OTC token",
+        "--otc-token", type=str, default="<star>", help="OTC token",
     )
 
     parser.add_argument(
