f0285bb4f51702505c8e450330164848112ec489
jinzr <zengrui.jin0@gmail.com>
2023-11-03 10:49:02 +0800

    Update optim.py

--- egs/librispeech/ASR/zipformer/optim.py
+++ egs/librispeech/ASR/zipformer/optim.py
@@ -456,6 +456,8 @@ class ScaledAdam(BatchedOptimizer):
                 quartiles.append(sorted_norms[index].item())
 
             median = quartiles[2]
+            if median - median != 0:
+                raise RuntimeError("Too many grads were not finite")
             threshold = clipping_scale * median
             first_state["model_norm_threshold"] = threshold
             percent_clipped = (
