86c3dbec0ed9e1581d5ebbbcb8851951083e4339
jinzr <zengrui.jin0@gmail.com>
2023-11-08 10:07:32 +0800

    Update prepare_lm_training_data.py

--- egs/multi_zh-hans/ASR/local/prepare_lm_training_data.py
+++ egs/multi_zh-hans/ASR/local/prepare_lm_training_data.py
@@ -101,7 +101,7 @@ def main():
             line = f.readline()
             line = tokenize_by_CJK_char(line)
             if line == "":
-                break
+                continue
 
             if step and processed % step == 0:
                 logging.info(f"Processed number of lines: {processed} ")
