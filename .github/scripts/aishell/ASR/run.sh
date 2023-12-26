83ac992569e80eec766a6caf38c4dbd401bf7ca5
Fangjun Kuang <csukuangfj@gmail.com>
2023-12-26 20:18:59 +0800

    more fixes

--- .github/scripts/aishell/ASR/run.sh
+++ .github/scripts/aishell/ASR/run.sh
@@ -66,7 +66,6 @@ function test_transducer_stateless3_2022_06_20() {
 
   echo "GITHUB_EVENT_NAME: ${GITHUB_EVENT_NAME}"
   echo "GITHUB_EVENT_LABEL_NAME: ${GITHUB_EVENT_LABEL_NAME}"
-  GITHUB_EVENT_NAME="schedule"
   if [[ x"${GITHUB_EVENT_NAME}" == x"schedule" || x"${GITHUB_EVENT_LABEL_NAME}" == x"run-decode"  ]]; then
     mkdir -p pruned_transducer_stateless3/exp
     ln -s $PWD/$repo/exp/pretrained.pt pruned_transducer_stateless3/exp/epoch-999.pt
@@ -97,7 +96,179 @@ function test_transducer_stateless3_2022_06_20() {
   rm -rf $repo
 }
 
+function test_zipformer_large_2023_10_24() {
+  log "CI testing large model"
+  repo_url=https://huggingface.co/zrjin/icefall-asr-aishell-zipformer-large-2023-10-24/
+  log "Downloading pre-trained model from $repo_url"
+  git clone $repo_url
+  repo=$(basename $repo_url)
+
+  log "Display test files"
+  tree $repo/
+  ls -lh $repo/test_wavs/*.wav
+
+  for method in modified_beam_search greedy_search fast_beam_search; do
+    log "$method"
+
+    ./zipformer/pretrained.py \
+      --method $method \
+      --context-size 1 \
+      --checkpoint $repo/exp/pretrained.pt \
+      --tokens $repo/data/lang_char/tokens.txt \
+      --num-encoder-layers 2,2,4,5,4,2 \
+      --feedforward-dim 512,768,1536,2048,1536,768 \
+      --encoder-dim 192,256,512,768,512,256 \
+      --encoder-unmasked-dim 192,192,256,320,256,192 \
+      $repo/test_wavs/BAC009S0764W0121.wav \
+      $repo/test_wavs/BAC009S0764W0122.wav \
+      $repo/test_wavs/BAC009S0764W0123.wav
+  done
+  rm -rf $repo
+}
+
+function test_zipformer_2023_10_24() {
+  repo_url=https://huggingface.co/zrjin/icefall-asr-aishell-zipformer-2023-10-24/
+  log "Downloading pre-trained model from $repo_url"
+  git clone $repo_url
+  repo=$(basename $repo_url)
+
+  log "Display test files"
+  tree $repo/
+  ls -lh $repo/test_wavs/*.wav
+
+
+  for method in modified_beam_search greedy_search fast_beam_search; do
+    log "$method"
+
+    ./zipformer/pretrained.py \
+      --method $method \
+      --context-size 1 \
+      --checkpoint $repo/exp/pretrained.pt \
+      --tokens $repo/data/lang_char/tokens.txt \
+      $repo/test_wavs/BAC009S0764W0121.wav \
+      $repo/test_wavs/BAC009S0764W0122.wav \
+      $repo/test_wavs/BAC009S0764W0123.wav
+  done
+  rm -rf $repo
+}
+
+function test_zipformer_small_2023_10_24() {
+  log "CI testing small model"
+  repo_url=https://huggingface.co/zrjin/icefall-asr-aishell-zipformer-small-2023-10-24/
+  log "Downloading pre-trained model from $repo_url"
+  git clone $repo_url
+  repo=$(basename $repo_url)
+
+  log "Display test files"
+  tree $repo/
+  ls -lh $repo/test_wavs/*.wav
+
+
+  for method in modified_beam_search greedy_search fast_beam_search; do
+    log "$method"
+
+    ./zipformer/pretrained.py \
+      --method $method \
+      --context-size 1 \
+      --checkpoint $repo/exp/pretrained.pt \
+      --tokens $repo/data/lang_char/tokens.txt \
+      --num-encoder-layers 2,2,2,2,2,2 \
+      --feedforward-dim 512,768,768,768,768,768 \
+      --encoder-dim 192,256,256,256,256,256 \
+      --encoder-unmasked-dim 192,192,192,192,192,192 \
+      $repo/test_wavs/BAC009S0764W0121.wav \
+      $repo/test_wavs/BAC009S0764W0122.wav \
+      $repo/test_wavs/BAC009S0764W0123.wav
+  done
+  rm -rf $repo
+}
+
+function test_transducer_stateless_modified_2022_03_01() {
+  repo_url=https://huggingface.co/csukuangfj/icefall-aishell-transducer-stateless-modified-2022-03-01
+
+  log "Downloading pre-trained model from $repo_url"
+  git lfs install
+  git clone $repo_url
+  repo=$(basename $repo_url)
+
+  log "Display test files"
+  tree $repo/
+  ls -lh $repo/test_wavs/*.wav
+
+  for sym in 1 2 3; do
+    log "Greedy search with --max-sym-per-frame $sym"
+
+    ./transducer_stateless_modified/pretrained.py \
+      --method greedy_search \
+      --max-sym-per-frame $sym \
+      --checkpoint $repo/exp/pretrained.pt \
+      --lang-dir $repo/data/lang_char \
+      $repo/test_wavs/BAC009S0764W0121.wav \
+      $repo/test_wavs/BAC009S0764W0122.wav \
+      $repo/test_wavs/BAC009S0764W0123.wav
+  done
+
+  for method in modified_beam_search beam_search; do
+    log "$method"
+
+    ./transducer_stateless_modified/pretrained.py \
+      --method $method \
+      --beam-size 4 \
+      --checkpoint $repo/exp/pretrained.pt \
+      --lang-dir $repo/data/lang_char \
+      $repo/test_wavs/BAC009S0764W0121.wav \
+      $repo/test_wavs/BAC009S0764W0122.wav \
+      $repo/test_wavs/BAC009S0764W0123.wav
+  done
+  rm -rf $repo
+}
+
+function test_transducer_stateless_modified_2_2022_03_01() {
+  repo_url=https://huggingface.co/csukuangfj/icefall-aishell-transducer-stateless-modified-2-2022-03-01
+
+  log "Downloading pre-trained model from $repo_url"
+  git lfs install
+  git clone $repo_url
+  repo=$(basename $repo_url)
+
+  log "Display test files"
+  tree $repo/
+  ls -lh $repo/test_wavs/*.wav
+
+  for sym in 1 2 3; do
+    log "Greedy search with --max-sym-per-frame $sym"
+
+    ./transducer_stateless_modified-2/pretrained.py \
+      --method greedy_search \
+      --max-sym-per-frame $sym \
+      --checkpoint $repo/exp/pretrained.pt \
+      --lang-dir $repo/data/lang_char \
+      $repo/test_wavs/BAC009S0764W0121.wav \
+      $repo/test_wavs/BAC009S0764W0122.wav \
+      $repo/test_wavs/BAC009S0764W0123.wav
+  done
+
+  for method in modified_beam_search beam_search; do
+    log "$method"
+
+    ./transducer_stateless_modified-2/pretrained.py \
+      --method $method \
+      --beam-size 4 \
+      --checkpoint $repo/exp/pretrained.pt \
+      --lang-dir $repo/data/lang_char \
+      $repo/test_wavs/BAC009S0764W0121.wav \
+      $repo/test_wavs/BAC009S0764W0122.wav \
+      $repo/test_wavs/BAC009S0764W0123.wav
+  done
+  rm -rf $repo
+}
+
 download_test_dev_manifests
 test_transducer_stateless3_2022_06_20
+test_zipformer_large_2023_10_24
+test_zipformer_2023_10_24
+test_zipformer_small_2023_10_24
+test_transducer_stateless_modified_2022_03_01
+test_transducer_stateless_modified_2_2022_03_01
 
 ls -lh
