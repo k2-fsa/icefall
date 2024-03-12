959906e9dcded13ef0618c6ea815a18ed3f19324
BannerWang <45093113+Banner-Wang@users.noreply.github.com>
2024-03-12 12:44:09 +0800

    Correct alimeeting download link (#1544)
    Co-authored-by: BannerWang <banner.wang@upblocks.io>

--- egs/alimeeting/ASR_v2/prepare.sh
+++ egs/alimeeting/ASR_v2/prepare.sh
@@ -12,7 +12,7 @@ use_gss=true  # Use GSS-based enhancement with MDM setting
 #
 #  - $dl_dir/alimeeting
 #     This directory contains the following files downloaded from
-#       https://openslr.org/62/
+#       https://openslr.org/119/
 #
 #     - Train_Ali_far.tar.gz
 #     - Train_Ali_near.tar.gz
