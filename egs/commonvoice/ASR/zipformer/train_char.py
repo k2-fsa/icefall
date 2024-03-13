e979bf5e93c349caf3431477f3570fa8fa4251e1
jinzr <zengrui.jin0@gmail.com>
2024-03-13 17:22:32 +0800

    Update train_char.py

--- egs/commonvoice/ASR/zipformer/train_char.py
+++ egs/commonvoice/ASR/zipformer/train_char.py
@@ -376,7 +376,7 @@ def get_params() -> AttributeDict:
             "valid_interval": 3000,  # For the 100h subset, use 800
             # parameters for zipformer
             "feature_dim": 80,
-            "subsampling_factor": 2,  # not passed in, this is fixed.
+            "subsampling_factor": 4,  # not passed in, this is fixed.
             "warm_step": 2000,
             "env_info": get_env_info(),
         }
