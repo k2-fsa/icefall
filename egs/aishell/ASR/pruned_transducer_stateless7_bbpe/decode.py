bf2f94346c04496e3b4c9c61dfa97a474124fcae
zr_jin <peter.jin.cn@gmail.com>
2024-03-18 11:57:47 +0800

    Enabling `char_level` and `compute_CER` for `aishell` recipe (#1554)
    * init fix
    
    Co-authored-by: Fangjun Kuang <csukuangfj@gmail.com>

--- egs/aishell/ASR/pruned_transducer_stateless7_bbpe/decode.py
+++ egs/aishell/ASR/pruned_transducer_stateless7_bbpe/decode.py
@@ -581,7 +581,7 @@ def save_results(
     for key, results in results_dict.items():
         recog_path = params.res_dir / f"recogs-{test_set_name}-{params.suffix}.txt"
         results = sorted(results)
-        store_transcripts(filename=recog_path, texts=results)
+        store_transcripts(filename=recog_path, texts=results, char_level=True)
         logging.info(f"The transcripts are stored in {recog_path}")
 
         # The following prints out WERs, per-word error statistics and aligned
@@ -594,7 +594,11 @@ def save_results(
 
         with open(errs_filename, "w") as f:
             wer = write_error_stats(
-                f, f"{test_set_name}-{key}", results_char, enable_log=True
+                f,
+                f"{test_set_name}-{key}",
+                results_char,
+                enable_log=True,
+                compute_CER=True,
             )
             test_set_wers[key] = wer
 
