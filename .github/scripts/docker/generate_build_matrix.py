1bd8a3113cf88e8f478682490247c3afe244b2ff
Fangjun Kuang <csukuangfj@gmail.com>
2024-04-01 12:01:13 +0800

    support torch 2.2.2 in docker image

--- .github/scripts/docker/generate_build_matrix.py
+++ .github/scripts/docker/generate_build_matrix.py
@@ -45,10 +45,13 @@ def get_torchaudio_version(torch_version):
 def get_matrix():
     k2_version = "1.24.4.dev20240223"
     kaldifeat_version = "1.25.4.dev20240223"
-    version = "20240318"
+    version = "20240401"
     python_version = ["3.8", "3.9", "3.10", "3.11", "3.12"]
-    torch_version = ["1.13.0", "1.13.1", "2.0.0", "2.0.1", "2.1.0", "2.1.1", "2.1.2"]
-    torch_version += ["2.2.0", "2.2.1"]
+    torch_version = []
+    torch_version += ["1.13.0", "1.13.1"]
+    torch_version += ["2.0.0", "2.0.1"]
+    torch_version += ["2.1.0", "2.1.1", "2.1.2"]
+    torch_version += ["2.2.0", "2.2.1", "2.2.2"]
 
     matrix = []
     for p in python_version:
@@ -62,10 +65,17 @@ def get_matrix():
             if version_gt(p, "3.11") and not version_gt(t, "2.1"):
                 continue
 
+            k2_version_2 = k2_version
+            kaldifeat_version_2 = kaldifeat_version
+
+            if t == "2.2.2":
+                k2_version_2 = "1.24.4.dev20240328"
+                kaldifeat_version_2 = "1.25.4.dev20240329"
+
             matrix.append(
                 {
-                    "k2-version": k2_version,
-                    "kaldifeat-version": kaldifeat_version,
+                    "k2-version": k2_version_2,
+                    "kaldifeat-version": kaldifeat_version_2,
                     "version": version,
                     "python-version": p,
                     "torch-version": t,
