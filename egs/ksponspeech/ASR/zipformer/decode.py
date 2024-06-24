031f8927961c667d1f713bc1340ba2888b157b4d
Seung Hyun Lee <whsqkaak@naver.com>
2024-06-24 16:28:09 +0900

    Reformat by black non-streaming zipformer recipe for ksponspeech (#1665)

--- egs/ksponspeech/ASR/zipformer/decode.py
+++ egs/ksponspeech/ASR/zipformer/decode.py
@@ -727,7 +727,11 @@ def save_results(
         )
         with open(errs_filename, "w") as f:
             cer = write_error_stats(
-                f, f"{test_set_name}-{key}", results, enable_log=True, compute_CER=True,
+                f,
+                f"{test_set_name}-{key}",
+                results,
+                enable_log=True,
+                compute_CER=True,
             )
             test_set_cers[key] = cer
 
