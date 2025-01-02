bce18ed5642d3a0838f0e6f7c6d545c0f69ad6cc
Fangjun Kuang <csukuangfj@gmail.com>
2025-01-02 10:00:34 +0800

    rename tostring_rgb to tostring_argb

--- egs/ljspeech/TTS/vits/utils.py
+++ egs/ljspeech/TTS/vits/utils.py
@@ -113,7 +113,7 @@ def plot_feature(spectrogram):
     plt.tight_layout()
 
     fig.canvas.draw()
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
+    data = np.fromstring(fig.canvas.tostring_argb(), dtype=np.uint8, sep="")
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     plt.close()
     return data
