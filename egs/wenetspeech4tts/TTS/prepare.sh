2e594c61a158c2b1c95e385fe339ed1fd72e52ba
root <zhangyuekai@foxmail.com>
2025-01-20 09:37:52 +0000

    undo changes

--- egs/wenetspeech4tts/TTS/prepare.sh
+++ egs/wenetspeech4tts/TTS/prepare.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
+
 set -eou pipefail
-
-
 
 # fix segmentation fault reported in https://github.com/k2-fsa/icefall/issues/674
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
