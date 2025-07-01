55d066433957a6b53f09c25472e7bb4a335eaf6e
Bailey Hirota <baileyhirota@icloud.com>
2025-07-01 21:04:18 +0900

    typos

--- egs/multi_ja_en/ASR/local/utils/asr_datamodule.py
+++ egs/multi_ja_en/ASR/local/utils/asr_datamodule.py
@@ -272,8 +272,8 @@ class MultiDatasetAsrDataModule:
                 max_duration=self.args.max_duration,
                 shuffle=self.args.shuffle,
                 num_buckets=self.args.num_buckets,
-                buffer_size=self.args.num_buckers * 2000,
-                shuffle_buffer_size=self.args.num_buckers * 5000,
+                buffer_size=self.args.num_buckets * 2000,
+                shuffle_buffer_size=self.args.num_buckets * 5000,
                 drop_last=self.args.drop_last,
             )
         else:
