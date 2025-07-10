6272827db3f3328533407be1f573a08ca463cd70
Bailey Hirota <baileyhirota@icloud.com>
2025-07-10 13:23:30 +0900

    update musan path

--- egs/multi_ja_en/ASR/local/utils/asr_datamodule.py
+++ egs/multi_ja_en/ASR/local/utils/asr_datamodule.py
@@ -202,7 +202,7 @@ class MultiDatasetAsrDataModule:
         if self.args.enable_musan:
             logging.info("Enable MUSAN")
             logging.info("About to get Musan cuts")
-            cuts_musan = load_manifest(self.args.manifest_dir / "musan_cuts.jsonl.gz")
+            cuts_musan = load_manifest(self.args.manifest_dir / "musan/musan_cuts.jsonl.gz")
             transforms.append(CutMix(cuts=cuts_musan, p=0.5, snr=(10,20), preserve_id=True))
         else:
             logging.info("Disable MUSAN")
