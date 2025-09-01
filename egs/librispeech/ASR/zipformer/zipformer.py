efae008c236f083d3341ae5a5d640466d67055e7
Daniel Povey <dpovey@gmail.com>
2025-09-01 17:44:10 +0800

    Adjust min_product threshold based on a formula.

--- egs/librispeech/ASR/zipformer/zipformer.py
+++ egs/librispeech/ASR/zipformer/zipformer.py
@@ -841,7 +841,11 @@ dropout:
 
         self.predict_loss = PredictLoss(dim)
 
-        self.min_product_loss = MinProductLoss(0.5)
+        ratio = 1.2 # require the "passed-through" dims be larger by a factor of 1.2 tyan the bypassed dims.
+        d_yes = encoder_layer.embed_dim
+        d_no = dim - encoder_layer.embed_dim
+        min_product = (d_yes * ratio) / (d_yes * ratio + d_no)
+        self.min_product_loss = MinProductLoss(min_product)
 
         self.cosine_loss = CosineSimilarityLoss(get_max_similarity(rank=dim, power=0.85))
 
