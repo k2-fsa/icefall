69a2494f16ebf9cbd065b0dbb4415550f08352a7
Daniel Povey <dpovey@gmail.com>
2025-09-09 15:25:21 +0800

    Reduce subsampling factor of central stack from 8 to 4, so there are 3 stacks with subsampling-factor=4.

--- egs/librispeech/ASR/zapformer/train.py
+++ egs/librispeech/ASR/zapformer/train.py
@@ -192,7 +192,7 @@ def add_model_arguments(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--downsampling-factor",
         type=str,
-        default="1,2,4,8,4,2",
+        default="1,2,4,4,4,2",
         help="Downsampling factor for each stack of encoder layers.",
     )
 
