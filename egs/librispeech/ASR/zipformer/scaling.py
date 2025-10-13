1430b0e02babaa123443349d9c0cfb0acf891bde
Daniel Povey <dpovey@gmail.com>
2025-10-13 15:18:25 +0800

    Take the formula tanh(sqrt(0.1**2 + x**2)) from 1338, and the scale min=0.75.

--- egs/librispeech/ASR/zipformer/scaling.py
+++ egs/librispeech/ASR/zipformer/scaling.py
@@ -334,9 +334,10 @@ class MaxEigLimiterFunction(torch.autograd.Function):
 
 
 def _exp_norm(x: Tensor, scale: Tensor, channel_dim: int):
-    # plot (0.15 + 0.87*x**0.5)^(1/0.5), x+0.075 for 0 <= x <= 1
-    x_norm = torch.mean(x ** 2, dim=channel_dim, keepdim=True).sqrt()
-    num = ((0.15 + x_norm ** 0.5) ** 2).tanh()
+    eps = 0.1
+    var = torch.mean(x ** 2, dim=channel_dim, keepdim=True)
+    x_norm, x_norm_witheps = var.sqrt(), (var + eps**2).sqrt()
+    num = x_norm_witheps.tanh()
     scales = num / x_norm
     scales = scale * scales
     return (x * scales)
@@ -428,7 +429,7 @@ class ExpNorm(torch.nn.Module):
             return _exp_norm(x, self.scale, self.channel_dim)
 
         scale = limit_param_value(
-            self.scale, min=0.4, max=2.5, training=self.training)
+            self.scale, min=0.75, max=2.5, training=self.training)
 
         ans = ExpNormFunction.apply(
             x, scale, self.channel_dim,
