10fb6f1be64109d247b28bccf136969b71d2c66c
Daniel Povey <dpovey@gmail.com>
2025-11-18 14:02:37 +0800

    Replace convolution in convolution module with something based on hilbert transform.

--- egs/librispeech/ASR/zipformer/zipformer.py
+++ egs/librispeech/ASR/zipformer/zipformer.py
@@ -555,7 +555,6 @@ class Zipformer2EncoderLayer(nn.Module):
     def forward(
         self,
         src: Tensor,
-        weight_proj: Tensor,
         pos_emb: Tensor,
         chunk_size: int = -1,
         attn_mask: Optional[Tensor] = None,
@@ -566,7 +565,6 @@ class Zipformer2EncoderLayer(nn.Module):
             Pass the input through the encoder layer.
             Args:
                 src: the sequence to the encoder (required): shape (seq_len, batch_size, embedding_dim).
-         weight_proj: to be passed to the convolution modules, of shape (max_conv_length, conv_params)
              pos_emb: (1, 2*seq_len-1, pos_emb_dim) or (batch_size, 2*seq_len-1, pos_emb_dim)
              chunk_size: the number of frames per chunk, of >= 0; if -1, no chunking.
              attn_mask: the attention mask, of shape (batch_size, seq_len, seq_len) or (seq_len, seq_len),
@@ -597,7 +595,7 @@ class Zipformer2EncoderLayer(nn.Module):
 
         src = src + self.self_attn(src, attn_weights, aux_loss_scale=0.1 * aux_loss_scale, src_key_padding_mask=src_key_padding_mask)
 
-        src = src + self.conv_module(src, weight_proj, chunk_size=chunk_size, src_key_padding_mask=src_key_padding_mask, aux_loss_scale=0.1 * aux_loss_scale)
+        src = src + self.conv_module(src, chunk_size=chunk_size, src_key_padding_mask=src_key_padding_mask, aux_loss_scale=0.1 * aux_loss_scale)
 
         src = src + self.feed_forward2(src, aux_loss_scale=0.1 * aux_loss_scale, src_key_padding_mask=src_key_padding_mask)
 
@@ -773,13 +771,6 @@ class Zipformer2Encoder(nn.Module):
                       dim=0))
 
 
-        conv_params = encoder_layer.conv_module.depthwise_conv.weight.shape[1]
-        max_conv_length = 255
-        self.weight_proj = nn.Parameter(torch.randn(max_conv_length, conv_params))
-        # scale weight_proj with a scale that's smaller for 'further-away-from-the-center' positions, since these positions
-        # will tend to have smaller weights.
-        self.register_buffer('weight_proj_scale', (1. / (2. + (torch.arange(max_conv_length) - (max_conv_length // 2)).abs())).unsqueeze(-1))
-
         self.copy_bypass = Identity()
 
 
@@ -822,12 +813,10 @@ class Zipformer2Encoder(nn.Module):
                                            min=-1.0, max=-0.5)
 
         src_with_bypass = residual_scale * src
-        weight_proj = self.weight_proj * self.weight_proj_scale
 
         for i, mod in enumerate(self.layers):
             src = mod(
                 src,
-                weight_proj,
                 pos_emb,
                 chunk_size=chunk_size,
                 attn_mask=attn_mask,
@@ -1630,138 +1619,23 @@ class ProjDepthwiseConv(nn.Module):
 
 
 
-class ProjDepthwiseConv(nn.Module):
-    def __init__(self,
-                 num_channels: int,
-                 params_per_channel: int,
-                 bias: bool = True):
+class Hilbert(nn.Module):
+    def __init__(self):
         super().__init__()
-        # initialize to identity function.
-        self.weight = nn.Parameter((params_per_channel ** -0.5) * torch.randn(num_channels, params_per_channel))
-        if bias:
-            self.bias = nn.Parameter(0.01 * torch.randn(num_channels))
-        else:
-            self.bias = None
 
 
     def forward(self,
-                x: Tensor,
-                weight_proj: Tensor) -> Tensor:
-        return self.forward_fft(x, weight_proj)
-        #a = self.forward_fft(x, weight_proj)
-        #b = self.forward_conv(x, weight_proj)
-        #diff = a - b
-        #def rms(x):
-        #    return (x**2).mean().sqrt()
-        #print(f"size={x.shape}, rms(a)={rms(a)}, rms(b)={rms(b)}, rms(diff)={rms(diff)}, rms(diff-last)={rms(diff[-1])}, rms(diff-first)={rms(diff[0])}")
-        #return a
-
-
-    def forward_conv(self,
-                     x: Tensor,
-                     weight_proj: Tensor) -> Tensor:
+                x: Tensor) -> Tensor:
         (seq_len, batch_size, num_channels) = x.shape
-        (_num_channels, params_per_channel) = self.weight.shape
-
-        weight_proj = weight_proj.t()
-        # weight_proj: (params_per_channel, conv_length)
-        assert weight_proj.shape[0] == params_per_channel
-        conv_length = weight_proj.shape[1]
-        assert conv_length % 2 == 1
-
-        # if convolution length is longer than seq_len, we can truncate the convolution by
-        # wrapping it around (so it will be the same as if we did the full convolution
-        # with circular padding)
-
-        if conv_length > seq_len:
-            wrapped_conv_length = seq_len
-
-            # 'multiple' is the number of 'wraps' we sum over.  this must be odd so
-            # that the original middle ends up in the middle after wrapping.
-            multiple = (conv_length + seq_len - 1) // seq_len
-            if multiple % 2 == 0:
-                multiple = multiple + 1  # need multiple to be odd.
-            padding = (seq_len * multiple) - conv_length
-            left_pad = padding // 2
-            right_pad = padding - left_pad
-            weight_proj = torch.nn.functional.pad(weight_proj, (left_pad, right_pad))
-
-            weight_proj = weight_proj.reshape(params_per_channel, multiple, seq_len).sum(dim=1)
-            # weight_proj: (num_channels, seq_len)
-            if seq_len % 2 == 0:
-                # even-length convolution will cause efficiency problems for conv1d, so we pad
-                # the convolution with a zero on the left (which would have been the side that
-                # was made shorter by the uneven padding).  The fact that it's zero won't matter
-                # because we'll just get the value from the wrapped around other side, due to
-                # circular padding.
-                weight_proj = torch.cat((torch.zeros(params_per_channel, 1, device=weight_proj.device, dtype=weight_proj.dtype),
-                                         weight_proj), dim=1)
-            conv_length = weight_proj.shape[1]
-
-
-        weight = torch.matmul(self.weight, weight_proj)
-        # weight: (num_channels, conv_length) ; note, conv_length may have been reduced to seq_len + 1 already.
-        padding = conv_length // 2  # note, conv_length will be odd.
-
-        # weight: (num_channels, conv_width); conv_width is odd.
-
-        x = x.permute(1, 2, 0)  # (batch, channels, width)
-        weight = weight.unsqueeze(1)  # (num_channels, 1, conv_width)
-
-        x = torch.nn.functional.pad(x, (padding, padding), mode='circular')
-        x = torch.nn.functional.conv1d(x, weight, self.bias, groups=num_channels)
-        x = x.permute(2, 0, 1) # (seq, batch, channels)
-        return x
-
-
-    def forward_fft(self,
-                    x: Tensor,
-                    weight_proj: Tensor) -> Tensor:
-        (seq_len, batch_size, num_channels) = x.shape
-        (_num_channels, params_per_channel) = self.weight.shape
-
-        weight_proj = weight_proj.t()
-        # weight_proj: (params_per_channel, conv_length)
-        assert weight_proj.shape[0] == params_per_channel
-        conv_length = weight_proj.shape[1]
-        assert conv_length % 2 == 1
-
-        # if convolution length is longer than seq_len, we can truncate the convolution by
-        # wrapping it around (so it will be the same as if we did the full convolution
-        # with circular padding)
-
-
-        middle = conv_length // 2
-        # pad the convolution so that its middle point is positioned at an exact multiple of seq_len,
-        # which will become position zero after circular summing; and so that the total length is an
-        # exact multiple of seq_len.
-        left_pad = (-middle) % seq_len  # caution if you translate this into C, this relies on python's definition.
-        right_pad = (-(conv_length + left_pad)) % seq_len
-        weight_proj = torch.nn.functional.pad(weight_proj, (left_pad, right_pad))
-
-        weight_proj = weight_proj.reshape(params_per_channel, -1, seq_len).sum(dim=1)
-        # weight_proj: (num_channels, seq_len).  Central point of conv is positioned
-        # at position zero.
-
-        weight = torch.matmul(self.weight, weight_proj)
-        # weight: (num_channels, seq_len).
-
-
-        x = x.permute(1, 2, 0)  # (batch_size, num_channels, seq_len)
-
-        both = torch.cat((x, weight.unsqueeze(0)), dim=0)
-        # both: (batch_size + 1, num_channels, seq_len)
 
         with torch.amp.autocast('cuda', enabled=False):
             # do it in float32 because non power of two seq_len is not supported in half precision.
-            both = torch.fft.rfft(both.to(torch.float32))
+            x = torch.fft.rfft(x.to(torch.float32), dim=0)
 
-            # multiplication in fourier space is the same as (circular) convolution.
-            x = both[:-1] * both[-1].conj()
+            x = x * 1j
 
-            x = torch.fft.irfft(x, n=seq_len)
+            x = torch.fft.irfft(x, n=seq_len, dim=0)
 
-        x = x.permute(2, 0, 1) + self.bias # (seq, batch, channels)
         return x
 
 
@@ -1802,21 +1676,24 @@ class ConvolutionModule(nn.Module):
         self.activation2 = Identity()  # for diagnostics
 
 
-        self.depthwise_conv = ProjDepthwiseConv(bottleneck_dim,
-                                                kernel_size)
+        self.hilbert = Hilbert()  # hilbert transform
 
-        self.out_proj = ActivationDropoutAndLinear(
+        # phase change
+        self.phase_shift = nn.Parameter(torch.randn(bottleneck_dim))
+
+        # bias that determines gain.
+        self.bias = nn.Parameter(0.01 * torch.randn(bottleneck_dim))
+
+
+        self.out_proj = ScaledLinear(
             bottleneck_dim,
             channels,
-            activation="SwashR",
-            dropout_p=0.0,
             initial_scale=0.05,
         )
 
     def forward(
         self,
         x: Tensor,
-        weight_proj: Tensor,
         src_key_padding_mask: Optional[Tensor] = None,
         chunk_size: int = -1,
         aux_loss_scale: float = 0.0,
@@ -1825,7 +1702,6 @@ class ConvolutionModule(nn.Module):
 
         Args:
             x: Input tensor (#time, batch, channels).
-  weight_proj: tensor of shape (max_conv_length, kernel_size), with max_conv_length > kernel_size; expands the size of the convolution.
            src_key_padding_mask: the mask for the src keys per batch (optional):
                (batch, #time), contains True in masked positions.
 
@@ -1845,7 +1721,21 @@ class ConvolutionModule(nn.Module):
         if src_key_padding_mask is not None:
             x = self.repeat_in_padding(x, src_key_padding_mask)
 
-        x = self.depthwise_conv(x, weight_proj)
+
+        x = torch.complex(x, self.hilbert(x).to(x.dtype))
+
+
+        x_abs = x.abs()
+
+        # change the phase of x by multiplying by a phase shift.
+        phase_shift = self.phase_shift
+        x = x * torch.polar(torch.ones_like(phase_shift), phase_shift)
+
+        eps = 1.0e-05
+        x_scale = (x_abs + self.bias) / (x_abs + eps)
+        x_scale = x_scale.clamp(max=4.0)  # this is to limit gain which should lead to gradient blowup.
+
+        x = x_scale * torch.real(x)
 
         x = self.out_proj(x)  # (time, batch, channels)
 
