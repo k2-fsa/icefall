e74fa5ff47d7585a264557b6ef60aa771d557700
Daniel Povey <dpovey@gmail.com>
2025-11-19 12:07:08 +0800

    Reduce num-layers from 6,8,22,8 to 5,7,20,9 and increase value-head-dim from 48 to 64.

--- egs/librispeech/ASR/zapformer/train.py
+++ egs/librispeech/ASR/zapformer/train.py
@@ -185,7 +185,7 @@ def add_model_arguments(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--num-encoder-layers",
         type=str,
-        default="6,7,20,9",
+        default="5,7,20,9",
         help="Number of zipformer encoder layers per stack, comma separated.",
     )
 
