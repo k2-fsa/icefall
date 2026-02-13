f6f7037e5d4eb71f2a254efffa3152e16c773644
Daniel Povey <dpovey@gmail.com>
2026-02-13 13:54:10 +0800

    Revert "Remove final RmsNorm on zipformer."
    This reverts commit 9985a618f2c4937384887c3e9f1394edaea57c09.

--- egs/librispeech/ASR/zipformer/zipformer.py
+++ egs/librispeech/ASR/zipformer/zipformer.py
@@ -170,6 +170,8 @@ class Zipformer2(EncoderInterface):
 
         self.encoders = nn.ModuleList(encoders)
 
+        self.out_norm = RmsNorm()
+
 
     def get_chunk_info(self) -> Tuple[int, int]:
         """
@@ -271,6 +273,8 @@ class Zipformer2(EncoderInterface):
 
         if od > 1:
             x_lens = (x_lens + od - 1) // od
+
+        x = self.out_norm(x)
 
         return x, x_lens
 
