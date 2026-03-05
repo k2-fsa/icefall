5d8e775f1549082675a07314ed054e39c904b463
Daniel Povey <dpovey@gmail.com>
2026-03-05 15:37:23 +0800

    Implement version of TransformedAdam with x3 decay, where row/column scales are applied before and after the x3 decay.

--- egs/librispeech/ASR/zipformer/optim.py
+++ egs/librispeech/ASR/zipformer/optim.py
@@ -428,17 +428,25 @@ def momentum_step(group, state, grad):
 
     stored_delta.add_(delta)
     if delta.ndim >= 3 and delta.numel() != delta.shape[0] * max(delta.shape[1:]):
-
-        delta_reshaped = stored_delta.reshape(get_matrix_shape(stored_delta.shape))
-
+        d = stored_delta.reshape(get_matrix_shape(stored_delta.shape))
         # decay by one quarter of the beta1-determined decay rate, leaving the rest to the x^3 decay.
         # this should be configurable.
-
         linear_decay_scale = 0.25
 
-        delta_reshaped.mul_(linear_decay_scale * beta1 + (1 - linear_decay_scale))
+        d.mul_(linear_decay_scale * beta1 + (1 - linear_decay_scale))
         excess_scale = 2.5
-        x3 = delta_reshaped * (((1 - beta1**2)**0.5) / excess_scale)   # normalized-scale version of stored_delta, times
+        x3 = d * (((1 - beta1**2)**0.5) / excess_scale)   # normalized-scale version of stored_delta, times
+
+        if "delta2_buffer0" not in state:
+            state["delta2_buffer0"] = torch.ones(d.shape[0], d.shape[1], 1, device=d.device, dtype=d.dtype)
+            state["delta2_buffer1"] = torch.ones(d.shape[0], 1, d.shape[2], device=d.device, dtype=d.dtype)
+        delta2_buffer0 = state["delta2_buffer0"]
+        delta2_buffer1 = state["delta2_buffer1"]
+
+        # we'll scale both before and after the cubing
+        row_col_scale = 1. / ((delta2_buffer0 + eps).sqrt() * (delta2_buffer1 + eps).sqrt())
+
+        x3 = x3 * row_col_scale   #note, we are before computing the cubed part.
 
         compute_prod3_inplace(x3)  # actually computes 3rd power of its arg divided by max(rows, cols)**2
         # the factor of 0.5 says we only want to go, at most, half the way to the point which
@@ -447,14 +455,27 @@ def momentum_step(group, state, grad):
         # the largest singular value; or to prevent the largest singular value from going to
         # zero if it does dominate.
         alpha = (0.5 * min_sum_scale(stored_delta, x3)).clamp(min=-1)
-        delta_reshaped.add_(x3 * alpha)
+        # we divide x3 by row_col_scale to "un-normalize".
+        d.add_(x3 * alpha / row_col_scale)
 
+        if random.random() < 0.0005:
+            rel_scale = (d ** 2).mean().sqrt() / ((1 - beta1**2)**-0.5)
+            logging.info(f"shape={stored_delta.shape}, rel_scale = {rel_scale.item()}")
+
+        if not stored_delta.untyped_storage() is d.untyped_storage():
+            stored_delta[:] = d.reshape(*stored_delta.shape)
+
+        beta = beta1  # use this beta for row/col scales
+        d = d * row_col_scale  # half-normalized d
+        assumed_scale = 0.5 * ((1 - beta1**2)**-0.5) # assumed scalie of d
+        d2 = (d / assumed_scale) ** 2
         if random.random() < 0.001:
-            rel_scale = (delta_reshaped ** 2).mean().sqrt() / ((1 - beta1**2)**-0.5)
-            logging.info(f"rel_scale = {rel_scale.item()}")
+            logging.info(f"shape={stored_delta.shape}, mean of normalized d2 is {d2.mean().item()}")
+        delta2_buffer0.mul_(beta).add_(d2.mean(dim=2, keepdim=True), alpha=(1 - beta))
+        delta2_buffer1.mul_(beta).add_(d2.mean(dim=1, keepdim=True), alpha=(1 - beta))
+        d = d * row_col_scale  # fully-normalized d
 
-        if not stored_delta.untyped_storage() is delta_reshaped.untyped_storage():
-            stored_delta[:] = delta_reshaped.reshape(*stored_delta.shape)
+        stored_delta = d.reshape(*stored_delta.shape)   # note: permanent buffer is not updated.
     else:
         stored_delta.mul_(beta1)
 
